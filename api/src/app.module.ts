d0eef9ef8b75f7f139c87c39c993fe9516767b1f
alexeh <alexurola@gmail.com>
2024-05-18 08:44:21 +0300

    dockerization

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,6 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from '@api/app.service';
 import { UsersModule } from '@api/modules/users/users.module';
+import { User } from '@shared/dto/users/user.entity';
+
+// TODO: The dist folder structure changes if some shared lib is imported in the api. Check this
+
+const user: User = new User();
 
 @Module({
   imports: [UsersModule],
