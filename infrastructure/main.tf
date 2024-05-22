d6e9d316eea095bf7e0ece8a3f540b406f9c3de7
alexeh <alexurola@gmail.com>
2024-05-22 08:26:14 +0300

    Create GH secrets and vars through TF

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,18 +4,25 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.14"
     }
+#     github = {
+#       source  = "integrations/github"
+#       version = "~> 4.0"
+#     }
 
   }
 
   // TF does not allow vars here. Use main vars or module outputs for other variables
   backend "s3" {
-    bucket = "4growth-terraform-state"
+    bucket = "4-growth-terraform-state"
     key = "state"
     region = "eu-west-3"
-    // TODO: Use a generic profile name
-    profile = "alex-4-growth"
-    dynamodb_table = "4growth-terraform-state-lock"
+    profile = "4-growth"
+    dynamodb_table = "4-growth-terraform-state-lock"
   }
+#   backend "local" {
+#     path = "./state.json"
+#   }
+
 
   required_version = "~> 1.3.2"
 }
@@ -43,4 +50,17 @@ module api_ecr {
 
 module "iam" {
   source = "./modules/iam"
+}
+
+
+module "github" {
+    source = "./modules/github"
+    repo_name = var.project_name
+    github_owner = var.github_owner
+    github_token = var.github_token
+    secret_map = {
+        TF_PROJECT_NAME                    = var.project_name
+        TF_PIPELINE_USER_ACCESS_KEY_ID     = module.iam.pipeline_user_access_key_id
+        TF_PIPELINE_USER_SECRET_ACCESS_KEY = module.iam.pipeline_user_access_key_secret
+    }
 }
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -21,6 +21,18 @@ variable "project_name" {
   description = "Short name of the project, will be used to prefix created resources"
 }
 
+variable "github_owner" {
+  type        = string
+  description = "Owner of the Github repository where the code is hosted"
+}
+
+variable "github_token" {
+  type        = string
+  description = "Github token to access the repository"
+}
+
+
+
 
 #variable "repo_name" {
 #  type        = string
