e772c87f2b81d46a4831312658b6c7f2ddf87ef4
alexeh <alexurola@gmail.com>
2024-05-22 10:03:37 +0300

    Add image build and push workflow

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,11 +4,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.14"
     }
-#     github = {
-#       source  = "integrations/github"
-#       version = "~> 4.0"
-#     }
-
   }
 
   // TF does not allow vars here. Use main vars or module outputs for other variables
@@ -19,9 +14,7 @@ terraform {
     profile = "4-growth"
     dynamodb_table = "4-growth-terraform-state-lock"
   }
-#   backend "local" {
-#     path = "./state.json"
-#   }
+
 
 
   required_version = "~> 1.3.2"
@@ -62,5 +55,7 @@ module "github" {
         TF_PROJECT_NAME                    = var.project_name
         TF_PIPELINE_USER_ACCESS_KEY_ID     = module.iam.pipeline_user_access_key_id
         TF_PIPELINE_USER_SECRET_ACCESS_KEY = module.iam.pipeline_user_access_key_secret
+        TF_CLIENT_REPOSITORY_NAME          = module.client_ecr.repository_name
+        TF_API_REPOSITORY_NAME             = module.api_ecr.repository_name
     }
 }
