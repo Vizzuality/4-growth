c8ecd93d5fe6958a07e39f36c070b112923e530c
alexeh <alexurola@gmail.com>
2024-05-23 08:39:57 +0300

    Adds EBS service, networking and certificate

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -20,6 +20,63 @@ terraform {
   required_version = "~> 1.3.2"
 }
 
+data "aws_vpc" "default_vpc" {
+  default = true
+}
+
+data "aws_availability_zones" "all_available_azs" {
+  state = "available"
+}
+
+# THIS IS TO FILTER THE AVAILABLE ZONES BY EC2 INSTANCE TYPE AVAILABILITY
+# returns zone ids that have the requested instance type available
+data "aws_ec2_instance_type_offerings" "azs_with_ec2_instance_type_offering" {
+  filter {
+    name   = "instance-type"
+    values = ["m5a.large"]
+  }
+
+  filter {
+    name   = "location"
+    values = data.aws_availability_zones.all_available_azs.zone_ids
+  }
+
+  location_type = "availability-zone-id"
+}
+
+# THIS IS TO FIND THE NAMES OF THOSE ZONES GIVEN BY IDS FROM ABOVE...
+# because we need the names to pass to the staging module
+data "aws_availability_zones" "azs_with_ec2_instance_type_offering" {
+  filter {
+    name   = "zone-id"
+    values = sort(data.aws_ec2_instance_type_offerings.azs_with_ec2_instance_type_offering.locations)
+  }
+}
+
+# THIS IS TO FILTER THE SUBNETS BY AVAILABILITY ZONES WITH EC2 INSTANCE TYPE AVAILABILITY
+# so that we know which subnets can be passed to the beanstalk resource without upsetting it
+data "aws_subnets" "subnets_with_ec2_instance_type_offering_map" {
+  for_each = toset(
+    data.aws_ec2_instance_type_offerings.azs_with_ec2_instance_type_offering.locations
+  )
+
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default_vpc.id]
+  }
+
+  filter {
+    name   = "availability-zone-id"
+    values = ["${each.value}"]
+  }
+}
+
+locals {
+  subnets_with_ec2_instance_type_offering_ids = sort([
+    for k, v in data.aws_subnets.subnets_with_ec2_instance_type_offering_map : v.ids[0]
+  ])
+}
+
 
 
 module state {
@@ -59,4 +116,24 @@ module "github" {
         TF_API_REPOSITORY_NAME             = module.api_ecr.repository_name
         TF_AWS_REGION                      = var.aws_region
     }
+}
+
+resource "aws_iam_service_linked_role" "elasticbeanstalk" {
+  aws_service_name = "elasticbeanstalk.amazonaws.com"
+}
+
+module "dev" {
+  source                                        = "./modules/env"
+  domain                                        = "4-growth.dev.vizzuality.com"
+  project                                       = var.project_name
+  environment                                   = "dev"
+  aws_region                                    = var.aws_region
+  vpc                                           = data.aws_vpc.default_vpc
+  subnet_ids                                    = local.subnets_with_ec2_instance_type_offering_ids
+  availability_zones                            = data.aws_availability_zones.azs_with_ec2_instance_type_offering.names
+  beanstalk_platform                            = "64bit Amazon Linux 2023 v4.3.2 running Docker"
+  beanstalk_tier                                = "WebServer"
+  ec2_instance_type                             = "m5a.large"
+  elasticbeanstalk_iam_service_linked_role_name = aws_iam_service_linked_role.elasticbeanstalk.name
+  repo_name                                     = var.project_name
 }
