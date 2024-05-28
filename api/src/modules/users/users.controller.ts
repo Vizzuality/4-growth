ca367e6828761836ece654b3c4b7091809f9ecbb
anamontiaga <amontiaga@gmail.com>
2024-05-28 12:19:39 +0200

    countries CRUD

--- api/src/modules/users/users.controller.ts
+++ api/src/modules/users/users.controller.ts
@@ -1,9 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 
 @Controller()
 export class UsersController {
-  @Get('users')
-  createUser() {
+  @Post('users')
+  create(): string {
     return 'This action adds a new user';
+  }
+
+  @Get('users')
+  findAll(): string {
+    return 'This action returns all users';
+  }
+
+  @Get('users/:id')
+  findOne(@Param('id') id: string): string {
+    return `This action returns a #${id} user`;
+  }
+
+  @Put('users/:id')
+  update(@Param('id') id: string) {
+    return `This action updates a #${id} user`;
+  }
+
+  @Delete('users/:id')
+  remove(@Param('id') id: string) {
+    return `This action removes a #${id} user`;
   }
 }
