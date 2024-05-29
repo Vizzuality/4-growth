04bab5346d36e16b81e84c4256bb42930fc49ffc
anamontiaga <amontiaga@gmail.com>
2024-05-29 16:43:35 +0200

    fix mobile version

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,7 +2,7 @@ import ComingSoon from "@/containers/coming-soon";
 
 export default function Home() {
   return (
-    <main className="bg-blue-900 flex min-h-screen flex-col items-center justify-between p-24 text-white text-2xl">
+    <main className="bg-blue-900 flex min-h-screen flex-col items-center justify-between">
       <ComingSoon />
     </main>
   );
