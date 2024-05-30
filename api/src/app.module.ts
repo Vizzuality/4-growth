16400d7d60aacc81381e63349d5595009c630cb6
alexeh <alexurola@gmail.com>
2024-05-30 09:27:00 +0300

    remove app config

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,29 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from '@api/app.service';
 import { UsersModule } from '@api/modules/users/users.module';
 import { CountriesModule } from '@api/modules/countries/countries.module';
-import { User } from '@shared/dto/users/user.entity';
-import { Country } from '@shared/dto/countries/country.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import * as config from '@shared/config/development.json';
-
-// TODO: refactor the conf to use
+import { typeOrmConfig } from '@api/typeorm.config';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: config.db.database,
-      entities: [User, Country],
-      synchronize: true,
-    }),
-
-    UsersModule,
-    CountriesModule,
-  ],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), UsersModule, CountriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
