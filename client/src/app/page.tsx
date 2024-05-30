ffaf30b1a2393b4bff2458a3ec2654476c04235b
anamontiaga <amontiaga@gmail.com>
2024-05-30 09:48:25 +0200

    remove non used styles

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,7 +2,7 @@ import ComingSoon from "@/containers/coming-soon";
 
 export default function Home() {
   return (
-    <main className="bg-blue-900 flex min-h-screen flex-col items-center justify-between">
+    <main className="bg-blue-900 min-h-screen">
       <ComingSoon />
     </main>
   );
