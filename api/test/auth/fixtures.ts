3ac3c1a9ca7149f42533337af38819ec373824f8
alexeh <alexurola@gmail.com>
2024-06-04 10:14:16 +0300

    adds testing framework and signup tests

---
+++ api/test/auth/fixtures.ts
@@ -0,0 +1,66 @@
+import { TestManager } from '../utils/test-manager';
+import * as request from 'supertest';
+import { SignUpDto } from '@shared/dto/auth/sign-up.dto';
+import { createUser } from '../utils/entity-mocks';
+import { User } from '@shared/dto/users/user.entity';
+
+export class AuthFixtures {
+  testManager: TestManager<any>;
+
+  constructor(testManager: TestManager<any>) {
+    this.testManager = testManager;
+  }
+
+  async GivenThereIsUserRegistered(): Promise<User> {
+    const user = await createUser(this.testManager.getDataSource(), {
+      email: 'test@email.com',
+    });
+    return user;
+  }
+
+  async WhenISignUpANewUserWithWrongPayload(
+    signUpDto: SignUpDto,
+  ): Promise<request.Response> {
+    return request(this.testManager.getApp().getHttpServer())
+      .post('/auth/sign-up')
+      .send(signUpDto);
+  }
+
+  ThenIShouldReceiveValidationErrors(response: request.Response): void {
+    expect(response.status).toBe(400);
+    expect(response.body).toEqual({
+      message: [
+        'email must be an email',
+        'password must be longer than or equal to 8 characters',
+      ],
+      error: 'Bad Request',
+      statusCode: 400,
+    });
+  }
+
+  ThenIShouldReceiveAEmailAlreadyExistError(response: request.Response) {
+    expect(response.status).toBe(409);
+    expect(response.body).toEqual({
+      message: 'Email test@email.com already exists',
+      error: 'Conflict',
+      statusCode: 409,
+    });
+  }
+
+  async WhenISignUpANewUser(signUpDto: SignUpDto): Promise<request.Response> {
+    return request(this.testManager.getApp().getHttpServer())
+      .post('/auth/sign-up')
+      .send(signUpDto);
+  }
+
+  async ThenANewUserAShouldBeCreated(newUser: Partial<User>) {
+    const user = await this.testManager
+      .getDataSource()
+      .getRepository(User)
+      .findOne({
+        where: { email: newUser.email },
+      });
+    expect(user.id).toBeDefined();
+    expect(user.email).toEqual(newUser.email);
+  }
+}
---
+++ api/test/utils/entity-mocks.ts
@@ -0,0 +1,16 @@
+import { genSalt, hash } from 'bcrypt';
+import { DataSource, DeepPartial, BaseEntity } from 'typeorm';
+import { User } from '@shared/dto/users/user.entity';
+
+export const createUser = async (
+  dataSource: DataSource,
+  additionalData: Partial<User>,
+) => {
+  const salt = await genSalt();
+  const defaultData: DeepPartial<User> = {
+    email: 'test@user.com',
+    password: await hash('12345678', salt),
+  };
+  const user = { ...defaultData, ...additionalData };
+  return dataSource.getRepository(User).save(user);
+};
