973ca1741039451c7c50e5210180bc872058adba
alexeh <alexurola@gmail.com>
2024-06-05 16:22:51 +0300

    Add global jwt guard

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -5,10 +5,12 @@ import { UsersModule } from '@api/modules/users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from '@api/typeorm.config';
 import { AuthModule } from '@api/modules/auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from '../guards/jwt-auth.guard';
 
 @Module({
   imports: [TypeOrmModule.forRoot(typeOrmConfig), UsersModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, { provide: APP_GUARD, useClass: JwtAuthGuard }],
 })
 export class AppModule {}
