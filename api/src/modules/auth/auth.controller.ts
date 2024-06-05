8e9818f0c53a4e2b8ea1ddca7ff66d67216934c9
alexeh <alexurola@gmail.com>
2024-06-05 16:22:51 +0300

    Add global jwt guard

--- api/src/modules/auth/auth.controller.ts
+++ api/src/modules/auth/auth.controller.ts
@@ -3,16 +3,19 @@ import { AuthService } from '@api/modules/auth/auth.service';
 import { SignUpDto } from '@shared/dto/auth/sign-up.dto';
 import { SignInDto } from '@shared/dto/auth/sign-in.dto';
 import { IAccessToken } from '@shared/dto/auth/access-token.interface';
+import { Public } from '../../../decorators/is-public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @Post('/sign-up')
   async signUp(@Body() dto: SignUpDto): Promise<void> {
     return this.authService.signUp(dto);
   }
 
+  @Public()
   @Post('/sign-in')
   async signIn(@Body() dto: SignInDto): Promise<IAccessToken> {
     return this.authService.signIn(dto);
