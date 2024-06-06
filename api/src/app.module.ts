0422456b0a05391531360b4c9f29e04e22f528f4
alexeh <alexurola@gmail.com>
2024-06-06 08:32:34 +0300

    format exception responses to JSONAPI

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -5,12 +5,17 @@ import { UsersModule } from '@api/modules/users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from '@api/typeorm.config';
 import { AuthModule } from '@api/modules/auth/auth.module';
-import { APP_GUARD } from '@nestjs/core';
-import { JwtAuthGuard } from '../guards/jwt-auth.guard';
+import { APP_FILTER, APP_GUARD } from '@nestjs/core';
+import { AllExceptionsFilter } from '@api/filters/all-exceptions.exception.filter';
+import { JwtAuthGuard } from '@api/guards/jwt-auth.guard';
 
 @Module({
   imports: [TypeOrmModule.forRoot(typeOrmConfig), UsersModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService, { provide: APP_GUARD, useClass: JwtAuthGuard }],
+  providers: [
+    AppService,
+    { provide: APP_GUARD, useClass: JwtAuthGuard },
+    { provide: APP_FILTER, useClass: AllExceptionsFilter },
+  ],
 })
 export class AppModule {}
