ca74aeb00483389e791010cdb38dc04ba087371d
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2024-06-06 17:35:45 +0200

    adds session provider

--- client/src/app/providers.tsx
+++ client/src/app/providers.tsx
@@ -3,13 +3,22 @@
 import { PropsWithChildren, useState } from "react";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
 
-export default function LayoutProviders({ children }: PropsWithChildren) {
+export default function LayoutProviders({
+  children,
+  session,
+}: PropsWithChildren<{ session: Session | null }>) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
     <>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      <SessionProvider session={session}>
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      </SessionProvider>
     </>
   );
 }
