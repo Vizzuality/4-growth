44f4dff41b1e4010a2e8cdb872f60f1b1ed78995
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2024-06-13 13:18:39 +0200

    e2e auth tests

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -1,15 +1,30 @@
 import { defineConfig, devices } from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
+const API_URL = 'http://localhost:4000';
+const APP_URL = 'http://localhost:3000';
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+    /* Run your local dev server before starting the tests */
+    webServer: [
+        {
+            command: 'pnpm --filter api run build && pnpm --filter api run start:prod',
+            url: API_URL,
+            reuseExistingServer: !process.env.CI,
+        },
+        {
+            command: 'pnpm --filter client run build && pnpm --filter client run start',
+            url: APP_URL,
+            reuseExistingServer: !process.env.CI,
+            env: {
+                NEXTAUTH_SECRET: '7YWG6xGr8PzPWGpZteadxdqGjm/uyu/Qi48ArBpG4ag=',
+                NEXTAUTH_URL: APP_URL,
+                NEXT_PUBLIC_API_URL: API_URL
+            },
+        },
+    ],
     testDir: './tests',
     /* Run tests in files in parallel */
     fullyParallel: true,
@@ -24,12 +39,10 @@ export default defineConfig({
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
-        // baseURL: 'http://127.0.0.1:3000',
-
+        baseURL: APP_URL,
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: 'on-first-retry',
     },
-
     /* Configure projects for major browsers */
     projects: [
         {
@@ -46,32 +59,5 @@ export default defineConfig({
             name: 'webkit',
             use: { ...devices['Desktop Safari'] },
         },
-
-        /* Test against mobile viewports. */
-        // {
-        //   name: 'Mobile Chrome',
-        //   use: { ...devices['Pixel 5'] },
-        // },
-        // {
-        //   name: 'Mobile Safari',
-        //   use: { ...devices['iPhone 12'] },
-        // },
-
-        /* Test against branded browsers. */
-        // {
-        //   name: 'Microsoft Edge',
-        //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-        // },
-        // {
-        //   name: 'Google Chrome',
-        //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-        // },
     ],
-
-    // /* Run your local dev server before starting the tests */
-    // // webServer: {
-    // //   command: 'npm run start',
-    // //   url: 'http://127.0.0.1:3000',
-    // //   reuseExistingServer: !process.env.CI,
-    // // },
 });
