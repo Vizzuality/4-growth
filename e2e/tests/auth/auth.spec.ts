be71cf9fa0943f37270acfe1bef42095205842e2
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2024-06-20 15:33:03 +0200

    updates e2e tests

--- e2e/tests/auth/auth.spec.ts
+++ e2e/tests/auth/auth.spec.ts
@@ -1,10 +1,16 @@
 import { test, expect } from "@playwright/test";
 import {E2eTestManager} from "@shared/lib/e2e-test-manager";
+import { CreateUserDto } from "@shared/dto/users/create-user.dto";
 
 let testManager: E2eTestManager;
 
 test.beforeAll(async () => {
     testManager = await E2eTestManager.load();
+    await testManager.clearDatabase();
+});
+
+test.beforeEach(async () => {
+    await testManager.clearDatabase();
 });
 
 test.afterEach(async () => {
@@ -16,50 +22,44 @@ test.afterAll(async () => {
 });
 
 test('an user signs up successfully', async ({ page }) => {
-    const user = {
+    const user: CreateUserDto = {
         email: "johndoe@test.com",
-        username: "John Doe",
         password: "password"   
     };
 
-    await page.goto('/auth/signin');
-
-    await page.getByRole('button', { name: /sign up instead/i }).click();
-
-    await page.getByLabel('Username').fill('John Doe');
-    await page.getByLabel('Email').fill(user.email);
-    await page.getByLabel('Password').fill(user.password);
-
-    await page.getByRole('button', { name: /submit/i }).click();
-
-    await page.getByRole('button', { name: /sign in instead/i }).click();
+    await page.goto('/auth/signup');
 
     await page.getByLabel('Email').fill(user.email);
     await page.getByLabel('Password').fill(user.password);
+    await page.getByRole('checkbox').check();
+    
+    await page.getByRole('button', { name: /sign up/i }).click();
 
-    await page.getByRole('button', { name: /submit/i }).click();
+    await page.waitForURL('/auth/signin');
+
+    await page.getByLabel('Email').fill(user.email);
+    await page.locator('input[type="password"]').fill(user.password);
+
+    await page.getByRole('button', { name: /log in/i }).click();
 
     await page.waitForURL('/profile')
 
     await expect(page.getByText(user.email)).toHaveText(user.email);
-    await expect(page.getByText(user.username)).toHaveText(user.username);
 });
 
 test('an user signs in successfully', async ({ page }) => {
-    const user = {
+    const user: CreateUserDto = {
         email: "jhondoe@test.com",
-        username: "John Doe",
         password: "12345678"   
     }
     
-    await testManager.createUser({ email: user.email, username: user.username });
+    await testManager.createUser({ email: user.email });
 
     await page.goto('/auth/signin');
     await page.getByLabel('Email').fill(user.email);
-    await page.getByLabel('Password').fill(user.password);   
-    await page.getByRole('button', { name: /submit/i }).click();
+    await page.locator('input[type="password"]').fill(user.password);
+    await page.getByRole('button', { name: /log in/i }).click();
 
     await page.waitForURL('/profile')
     await expect(page.getByText(user.email)).toHaveText(user.email);
-    await expect(page.getByText(user.username)).toHaveText(user.username);
 })
