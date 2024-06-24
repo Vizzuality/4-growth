65150cb4e8cd65c70e72e33aea94565611ea0cbb
alexeh <alexurola@gmail.com>
2024-06-24 11:07:52 +0200

    Add local auth guard for login + update password flow

--- api/src/modules/auth/auth.service.ts
+++ api/src/modules/auth/auth.service.ts
@@ -1,53 +1,63 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '@api/modules/users/users.service';
 import { SignUpDto } from '@shared/dto/auth/sign-up.dto';
-import { CryptService } from '@api/modules/auth/crypt/crypt.service';
+import { PasswordService } from '@api/modules/auth/crypt/password.service';
 import { JwtService } from '@nestjs/jwt';
-import { SignInDto } from '@shared/dto/auth/sign-in.dto';
 import { JwtPayload } from '@api/modules/auth/jwt-payload.interface';
 import { IAccessToken } from '@shared/dto/auth/access-token.interface';
+import { UpdateUserPasswordDto } from '@shared/dto/users/update-user-password.dto';
+import { User } from '@shared/dto/users/user.entity';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly usersService: UsersService,
-    private readonly crypto: CryptService,
+    private readonly passwordService: PasswordService,
     private readonly jwtService: JwtService,
   ) {}
 
   async signUp(dto: SignUpDto): Promise<void> {
-    const passwordHash = await this.crypto.hashPassword(dto.password);
+    const passwordHash = await this.passwordService.hashPassword(dto.password);
     await this.usersService.createUser({
       email: dto.email,
       password: passwordHash,
     });
   }
 
-  async signIn(dto: SignInDto): Promise<IAccessToken> {
-    const user = await this.usersService.findByEmail(dto.email);
-    // TODO: Remove this logic when we apply global Auth guard
-    if (!user) {
-      throw new UnauthorizedException('Please check your login credentials');
+  async validateUser(email: string, password: string): Promise<User> {
+    const user = await this.usersService.findByEmail(email);
+    if (
+      user &&
+      (await this.passwordService.comparePassword(password, user.password))
+    ) {
+      return user;
     }
-    const isPasswordValid = await this.crypto.comparePassword(
-      dto.password,
-      user.password,
-    );
-    if (!isPasswordValid) {
-      throw new UnauthorizedException('Please check your login credentials');
-    }
+    throw new UnauthorizedException(`Invalid credentials`);
+  }
+
+  async signIn(user: User): Promise<IAccessToken> {
     const payload: JwtPayload = { email: user.email };
     const accessToken: string = this.jwtService.sign(payload);
     const { password, ...userWithoutPassword } = user;
     return { user: { ...userWithoutPassword }, accessToken };
   }
 
-  async userAlreadyExists(email: string): Promise<boolean> {
-    try {
-      await this.usersService.findByEmail(email);
-      return true;
-    } catch (e) {
-      return false;
+  async updatePassword(
+    userid: string,
+    dto: UpdateUserPasswordDto,
+  ): Promise<void> {
+    const user = await this.usersService.findOneBy(userid);
+    const isPasswordValid = await this.passwordService.comparePassword(
+      dto.currentPassword,
+      user.password,
+    );
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Current password is incorrect');
     }
+    const newPasswordHash = await this.passwordService.hashPassword(
+      dto.newPassword,
+    );
+    user.password = newPasswordHash;
+    await this.usersService.update(userid, user);
   }
 }
--- shared/dto/users/update-user.dto.ts
+++ shared/dto/users/update-user.dto.ts
@@ -1,15 +1,7 @@
 import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class UpdateUserDto {
-  @IsString()
-  @IsOptional()
-  username: string;
-
   @IsEmail()
   @IsNotEmpty()
   email: string;
-
-  @IsString()
-  @IsOptional()
-  password: string;
 }
