42b0f62dcecf18ee05e9ca47fbcbc69e11456c6e
alexeh <alexurola@gmail.com>
2024-06-24 11:07:52 +0200

    Add delete me endpoint, remove unused tests

--- api/test/users/fixtures.ts
+++ api/test/users/fixtures.ts
@@ -33,6 +33,14 @@ export class UserFixtures {
       .set('Authorization', `Bearer ${token}`);
   }
 
+  async WhenIQueryTheDeleteMeEndpoint(
+    token: string,
+  ): Promise<request.Response> {
+    return request(this.testManager.getApp().getHttpServer())
+      .delete(API_ROUTES.users.handlers.me.getRoute())
+      .set('Authorization', `Bearer ${token}`);
+  }
+
   ThenIShouldReceiveMyUserInformation(
     response: request.Response,
     existingUser: User,
@@ -41,9 +49,53 @@ export class UserFixtures {
     expect(response.body.email).toEqual(existingUser.email);
   }
 
-  ThenIShouldReceiveAUnauthorizedError(response: request.Response) {
+  ThenIShouldReceiveAUnauthorizedError(
+    response: request.Response,
+    customErrorMessages?: string,
+  ) {
     expect(response.status).toBe(401);
-    expect(response.body.errors[0].title).toEqual('Unauthorized');
+    expect(response.body.errors[0].title).toEqual(
+      customErrorMessages || 'Unauthorized',
+    );
+  }
+
+  async ThenIShouldBeAbleToLoginWithMyNewPassword(dto: SignInDto) {
+    const { email, password } = dto;
+    const response = await this.testManager
+      .request()
+      .post(API_ROUTES.auth.handlers.signIn.getRoute())
+      .send({ email, password });
+    expect(response.status).toBe(201);
+    expect(response.body.accessToken).toBeDefined();
+    expect(response.body.user.email).toEqual(email);
+  }
+
+  async AndIShouldNotBeAbleToLoginWithMyOldPassword(dto: SignInDto) {
+    const { email, password } = dto;
+    const response = await this.testManager
+      .request()
+      .post(API_ROUTES.auth.handlers.signIn.getRoute())
+      .send({ email, password });
+    expect(response.status).toBe(401);
+    expect(response.body.errors[0].title).toEqual('Invalid credentials');
+  }
+
+  async ThenIShouldNotBeAbleToLoginInWithMyCredentials(dto: SignInDto) {
+    const { email, password } = dto;
+    const response = await this.testManager
+      .request()
+      .post(API_ROUTES.auth.handlers.signIn.getRoute())
+      .send({ email, password });
+    expect(response.status).toBe(401);
+    expect(response.body.errors[0].title).toEqual('Invalid credentials');
+  }
+
+  async AndMyUserShouldBeDeleted(userId: string) {
+    const user = await this.testManager
+      .getDataSource()
+      .getRepository(User)
+      .findOne({ where: { id: userId } });
+    expect(user).toBeNull();
   }
 
   async ThenIShouldBeAbleToLoginWithMyNewPassword(dto: SignInDto) {
