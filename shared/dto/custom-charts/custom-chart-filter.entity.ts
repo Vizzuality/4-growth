28f81f9d111545514ccc08f5f325301fd5df002a
alexeh <alexurola@gmail.com>
2024-07-02 10:04:15 +0200

    Update data model: Add custom charts

---
+++ shared/dto/custom-charts/custom-chart-filter.entity.ts
@@ -0,0 +1,25 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { CustomChart } from '@shared/dto/custom-charts/custom-chart.entity';
+import { JoinColumn } from 'typeorm';
+
+export enum CHART_FILTER_ATTRIBUTES {
+  COUNTRY = 'country',
+  OPERATIONAL_AREAS = 'operational_areas',
+  TECHNOLOGY_TYPES = 'technology_types',
+}
+
+@Entity({ name: 'chart_filters' })
+export class ChartFilter {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'enum', enum: CHART_FILTER_ATTRIBUTES })
+  attribute: CHART_FILTER_ATTRIBUTES;
+
+  @Column({ type: 'varchar' })
+  value: string;
+
+  @ManyToOne(() => CustomChart, (customChart) => customChart.chartFilters)
+  @JoinColumn({ name: 'custom_chart_id' })
+  customChart: CustomChart;
+}
---
+++ shared/dto/custom-charts/custom-chart.entity.ts
@@ -0,0 +1,58 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { User } from '@shared/dto/users/user.entity';
+import { ChartFilter } from '@shared/dto/custom-charts/custom-chart-filter.entity';
+
+export enum CUSTOM_CHART_TYPE {
+  MAP = 'map',
+  BAR = 'bar',
+  DOUGHNUT = 'doughnut',
+  STACKED_BAR = 'stacked_bar',
+}
+
+// TODO: We don't really know if this will be a enum, a table, or where to get the values from
+
+export enum INDICATORS {
+  DIGITAL_TECHNOLOGIES = 'digital_technologies',
+  GOALS_OR_CHALLENGES = 'goals_or_challenges',
+}
+
+@Entity({ name: 'custom_charts' })
+export class CustomChart {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar' })
+  name: string;
+
+  @Column({
+    type: 'enum',
+    enum: CUSTOM_CHART_TYPE,
+    default: CUSTOM_CHART_TYPE.BAR,
+  })
+  type: CUSTOM_CHART_TYPE;
+
+  @Column({ type: 'enum', enum: INDICATORS })
+  indicator: INDICATORS;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+
+  @ManyToOne(() => User, (user) => user.customCharts)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @OneToMany(() => ChartFilter, (chartFilter) => chartFilter.customChart)
+  chartFilters: ChartFilter[];
+}
