3385e160007694c351fb18e3560dd7a4dec1e305
alexeh <alexurola@gmail.com>
2024-07-02 10:11:35 +0200

    centralize db entity registry

--- api/src/typeorm.config.ts
+++ api/src/typeorm.config.ts
@@ -1,8 +1,6 @@
 import { DataSourceOptions } from 'typeorm';
 import { AppConfig } from '@api/utils/app-config';
-import { User } from '@shared/dto/users/user.entity';
-import { CustomChart } from '@shared/dto/custom-charts/custom-chart.entity';
-import { ChartFilter } from '@shared/dto/custom-charts/custom-chart-filter.entity';
+import { DB_ENTITIES } from '@shared/lib/db-entities';
 
 /**
  * TypeORM configuration.
@@ -17,6 +15,6 @@ export const typeOrmConfig: DataSourceOptions = {
   username: dbConfig.username,
   password: dbConfig.password,
   database: dbConfig.database,
-  entities: [User, CustomChart, ChartFilter],
+  entities: DB_ENTITIES,
   synchronize: true,
 };
