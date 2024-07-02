62a777bb798b69fa736c0eeac838562c4b6ea0ea
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2024-07-02 13:10:47 +0200

    NODE_ENV test in API

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     /* Run your local dev server before starting the tests */
     webServer: [
         {
-            command: 'pnpm --filter api run build && pnpm --filter api run start:prod',
+            command: 'pnpm --filter api run build && NODE_ENV=test pnpm --filter api run start:prod',
             url: API_URL,
             reuseExistingServer: !process.env.CI,
         },
