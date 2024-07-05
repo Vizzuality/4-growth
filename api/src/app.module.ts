4dbb7e99e38c51f32c7100100cdf4719e2dc0b39
alexeh <alexurola@gmail.com>
2024-07-05 06:59:11 +0200

    workaround inject ssl conf to typeporm based on env

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,14 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from '@api/app.service';
 import { UsersModule } from '@api/modules/users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmConfig } from '@api/typeorm.config';
+import { getTypeORMConfig } from '@api/typeorm.config';
 import { AuthModule } from '@api/modules/auth/auth.module';
 import { APP_FILTER, APP_GUARD } from '@nestjs/core';
 import { AllExceptionsFilter } from '@api/filters/all-exceptions.exception.filter';
 import { JwtAuthGuard } from '@api/guards/jwt-auth.guard';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), UsersModule, AuthModule],
+  imports: [TypeOrmModule.forRoot(getTypeORMConfig()), UsersModule, AuthModule],
   controllers: [AppController],
   providers: [
     AppService,
