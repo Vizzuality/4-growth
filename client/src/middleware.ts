0288c0b76f5538fa0df5f18cabd2a3c707d22c5e
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2024-07-08 09:46:33 +0200

    adds health check endpoint

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -52,7 +52,8 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
+     * - health (health check endpoint)
      */
-    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|health).*)",
   ],
 };
