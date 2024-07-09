50e8396b595529de7f71dad0a991fbd1a2e678da
alexeh <alexurola@gmail.com>
2024-07-09 08:38:48 +0200

    introduce base crud service

--- api/src/utils/app-base.service.ts
+++ api/src/utils/app-base.service.ts
@@ -4,29 +4,9 @@ import {
   FetchSpecification,
 } from 'nestjs-base-service';
 
-import * as JSONAPISerializer from 'jsonapi-serializer';
 import { Repository } from 'typeorm';
-import { Serializer } from 'jsonapi-serializer';
 import { Logger } from '@nestjs/common';
-
-export class PaginationMeta {
-  totalPages: number;
-  totalItems: number;
-  size: number;
-  page: number;
-
-  constructor(paginationMeta: {
-    totalPages: number;
-    totalItems: number;
-    size: number;
-    page: number;
-  }) {
-    this.totalItems = paginationMeta.totalItems;
-    this.totalPages = paginationMeta.totalPages;
-    this.size = paginationMeta.size;
-    this.page = paginationMeta.page;
-  }
-}
+import { PaginationMeta } from '@shared/dto/global/api-response.dto';
 
 export abstract class AppBaseService<
   // eslint-disable-next-line @typescript-eslint/ban-types
@@ -43,42 +23,6 @@ export abstract class AppBaseService<
   ) {
     super(repository, alias, { idProperty });
     this.logger = new Logger(alias);
-  }
-
-  /**
-   * @debt Add proper typing.
-   */
-
-  async serialize(
-    entities: Partial<Entity> | (Partial<Entity> | undefined)[],
-    paginationMeta?: PaginationMeta,
-  ): Promise<any> {
-    const serializer: Serializer = new JSONAPISerializer.Serializer(
-      this.pluralAlias,
-      {
-        meta: paginationMeta,
-      },
-    );
-
-    return serializer.serialize(entities);
-  }
-
-  /**
-   * Curried wrapper for findAllPaginated(), defaulting to requesting raw
-   * results rather than entities.
-   */
-  async findAllPaginatedRaw(
-    fetchSpecification?: FetchSpecification,
-    info?: Info,
-  ): Promise<{
-    data: (Partial<Entity> | undefined)[];
-    metadata: PaginationMeta | undefined;
-  }> {
-    const entitiesAndCount: [Partial<Entity>[], number] = await this.findAllRaw(
-      fetchSpecification,
-      info,
-    );
-    return this._paginate(entitiesAndCount, fetchSpecification);
   }
 
   async findAllPaginated(
