51308b6a4333a2e8ab620d4063504eca05acccbf
alexeh <alexurola@gmail.com>
2024-07-12 08:31:25 +0200

    auth zod schema validation

--- shared/schemas/auth.schemas.ts
+++ shared/schemas/auth.schemas.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
-export const signInSchema = z.object({
+export const SignInSchema = z.object({
   email: z
     .string({ message: 'Email is required' })
     .min(1, 'Email is required')
@@ -10,4 +10,20 @@ export const signInSchema = z.object({
     .min(1, 'Password is required')
     .min(8, 'Password must contain at least 8 characters')
     .max(32, 'Password must be less than 32 characters'),
+});
+
+export const SignUpSchema = z.object({
+  email: z
+    .string({ message: 'Email is required' })
+    .min(1, 'Email is required')
+    .email('Invalid email'),
+  password: z
+    .string({ message: 'Password is required' })
+    .min(1, 'Password is required')
+    .min(8, 'Password must be more than 8 characters')
+    .max(32, 'Password must be less than 32 characters'),
+  // TODO: Check with FE how to handle this, the contract does not need to know about this
+  // privacyPolicy: z.boolean().refine((value) => value === true, {
+  //   message: 'Privacy policy must be accepted',
+  // }),
 });
