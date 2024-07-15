b28c21478f513d8c6c9cbd2a5f46caa4b29ef207
alexeh <alexurola@gmail.com>
2024-07-15 07:16:01 +0200

    Fix signin endpoint localguard

--- api/src/modules/auth/auth.controller.ts
+++ api/src/modules/auth/auth.controller.ts
@@ -1,10 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards } from '@nestjs/common';
 import { AuthService } from '@api/modules/auth/auth.service';
 import { Public } from '@api/decorators/is-public.decorator';
 import { GetUser } from '@api/decorators/get-user.decorator';
 import { User } from '@shared/dto/users/user.entity';
 import { tsRestHandler, TsRestHandler } from '@ts-rest/nest';
 import { authContract as c } from '@shared/contracts/auth.contract';
+import { LocalAuthGuard } from '@api/guards/local-auth.guard';
 
 @Controller()
 export class AuthController {
@@ -23,6 +24,7 @@ export class AuthController {
   }
 
   @Public()
+  @UseGuards(LocalAuthGuard)
   @TsRestHandler(c.signIn)
   async signIn(@GetUser() user: User): Promise<any> {
     return tsRestHandler(c.signIn, async () => {
