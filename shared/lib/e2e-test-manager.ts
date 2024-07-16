9a3baf9170c4a24d80563f3e863767b5db2fe1c0
alexeh <alexurola@gmail.com>
2024-07-16 08:25:43 +0200

    extend e2e test manager

--- shared/lib/e2e-test-manager.ts
+++ shared/lib/e2e-test-manager.ts
@@ -16,15 +16,17 @@ const AppDataSource = new DataSource({
 
 export class E2eTestManager {
   dataSource: DataSource;
+  page: any;
 
-  constructor(dataSource: DataSource) {
+  constructor(dataSource: DataSource, page?: any) {
     this.dataSource = dataSource;
+    this.page = page;
   }
 
-  static async load() {
+  static async load(page?: any) {
     await AppDataSource.initialize();
 
-    return new E2eTestManager(AppDataSource);
+    return new E2eTestManager(AppDataSource, page);
   }
 
   async clearDatabase() {
@@ -36,10 +38,35 @@ export class E2eTestManager {
   }
 
   async close() {
+    if (this.page) {
+      await this.page.close();
+    }
     await this.dataSource.destroy();
   }
 
   async createUser(additionalData?: Partial<User>) {
     return createUser(this.dataSource, additionalData);
+  }
+
+  getPage() {
+    if (!this.page) throw new Error('Playwright Page is not initialized');
+    return this.page;
+  }
+
+  async login(user?: User) {
+    if (!user) {
+      user = await this.createUser();
+    }
+    await this.page.goto('/auth/signin');
+    await this.page.getByLabel('Email').fill(user.email);
+    await this.page.locator('input[type="password"]').fill(user.password);
+    await this.page.getByRole('button', { name: /log in/i }).click();
+    await this.page.waitForURL('/profile');
+    return user;
+  }
+
+  async logout() {
+    await this.page.goto('/auth/api/signout');
+    await this.page.getByRole('button', { name: 'Sign out' }).click();
   }
 }
