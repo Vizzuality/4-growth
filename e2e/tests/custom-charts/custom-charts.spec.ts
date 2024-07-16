c482ba5499794895955c6c2e2ed483a3c4bc2578
alexeh <alexurola@gmail.com>
2024-07-16 09:31:12 +0200

    add custom chart e2e tests

---
+++ e2e/tests/custom-charts/custom-charts.spec.ts
@@ -0,0 +1,79 @@
+import { test, expect, Page } from '@playwright/test';
+import { E2eTestManager } from '@shared/lib/e2e-test-manager';
+import { User } from '@shared/dto/users/user.entity';
+import { CustomChart } from '@shared/dto/custom-charts/custom-chart.entity';
+
+test.describe.configure({ mode: 'serial' });
+
+test.describe('Custom Charts E2E', () => {
+  let testManager: E2eTestManager;
+  let page: Page;
+  let user: User;
+
+  test.beforeAll(async ({ browser }) => {
+    page = await browser.newPage();
+    testManager = await E2eTestManager.load(page);
+  });
+
+  test.beforeEach(async () => {
+    user = await testManager.login();
+  });
+
+  test.afterEach(async () => {
+    await testManager.clearDatabase();
+  });
+
+  test.afterAll(async () => {
+    await testManager.logout();
+    await testManager.close();
+  });
+
+  test.describe('List Custom Charts', () => {
+    let customCharts: CustomChart[] = [];
+    test.beforeEach(async () => {
+      for (const n of Array(10).keys()) {
+        customCharts.push(
+          await testManager.mocks().createCustomChart(user, {
+            name: `Test chart ${n}`,
+          })
+        );
+      }
+    });
+    test('a user can see a list of custom charts', async () => {
+      // TODO: Implement test for listing created charts
+    });
+  });
+  test.describe('Rename Custom Chart', () => {
+    test('a user can rename a custom chart', async () => {
+      const customChart = await testManager.mocks().createCustomChart(user);
+      await page.goto('/profile');
+      await page
+        .getByRole('cell', { name: customChart.type })
+        .getByRole('button')
+        .click();
+      await page.getByRole('button', { name: 'Rename' }).click();
+      await page.locator('input[type="text"]').fill('Cahnge');
+      await page.locator('input[type="text"]').press('Enter');
+      await expect(page.getByRole('status').first()).toHaveText(
+        'Visualization updated successfully.'
+      );
+    });
+  });
+  test.describe('Delete Custom Chart', () => {
+    test('a user can delete a custom chart', async () => {
+      const customChart = await testManager.mocks().createCustomChart(user);
+      await page.goto('/profile');
+      await page
+        .getByRole('cell', { name: customChart.type })
+        .getByRole('button')
+        .click();
+      await page.getByRole('button', { name: 'Delete', exact: true }).click();
+      await page.getByRole('button', { name: 'Delete visualization' }).click();
+      await expect(page.getByRole('status').first()).toHaveText(
+        'Visualization removed successfully.'
+      );
+      // await page.getByRole('status').click();
+      // await page.getByText('Visualization removed').click();
+    });
+  });
+});
