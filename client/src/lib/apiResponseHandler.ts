28f30895f8e04636d0d87d8f8645f6c1b60d24e5
alexeh <alexurola@gmail.com>
2024-07-23 08:02:46 +0200

    add apiResponseToastHook

---
+++ client/src/lib/apiResponseHandler.ts
@@ -0,0 +1,34 @@
+import { JSONAPIErrorOptions } from "@shared/dto/errors/json-api.error";
+
+import { useToast } from "@/components/ui/use-toast";
+
+export const useApiResponseToast = () => {
+  const { toast } = useToast();
+  const apiResponseToast = (
+    response: any,
+    options: { successMessage: string; customErrorMessage?: string },
+  ) => {
+    console.log({ response });
+    if (response.status >= 200 && response.status < 300) {
+      console.log("entra al toast de success");
+      toast({ description: options.successMessage });
+      return;
+    }
+    if (response.status >= 400 && response.status < 500) {
+      options.customErrorMessage
+        ? toast({
+            variant: "destructive",
+            description: options.customErrorMessage,
+          })
+        : response.body.errors.forEach((error: JSONAPIErrorOptions) => {
+            toast({ description: error.title });
+          });
+      return;
+    }
+    toast({
+      variant: "destructive",
+      description: "Something went wrong. Please try again.",
+    });
+  };
+  return { apiResponseToast, toast };
+};
