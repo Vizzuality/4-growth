26fc4be997d051c68f3e41f7a7ea5ca4beddc448
alexeh <alexurola@gmail.com>
2024-07-24 08:54:24 +0200

    send contact email from backend

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpStatus } from '@nestjs/common';
 import { AppService } from '@api/app.service';
+import { tsRestHandler, TsRestHandler } from '@ts-rest/nest';
+import { contactContract as c } from '@shared/contracts/contact.contract';
+import { Public } from '@api/decorators/is-public.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Public()
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Public()
+  @TsRestHandler(c.contact)
+  async recoverPassword(): Promise<any> {
+    return tsRestHandler(c.contact, async ({ body }) => {
+      await this.appService.contact(body);
+      return { body: null, status: HttpStatus.CREATED };
+    });
   }
 }
--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ContactForm, ContactMailer } from '@api/contact.mailer';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly contactMailer: ContactMailer) {}
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async contact(contactForm: ContactForm): Promise<void> {
+    await this.contactMailer.sendContactMail(contactForm);
   }
 }
