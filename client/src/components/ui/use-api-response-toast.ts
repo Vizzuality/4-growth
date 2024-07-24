5acc2a2fea5a8176513a70abc35f5e3a80cb49da
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2024-07-24 10:50:04 +0200

    fixing types - try

--- client/src/components/ui/use-api-response-toast.ts
+++ client/src/components/ui/use-api-response-toast.ts
@@ -2,36 +2,49 @@ import { JSONAPIErrorOptions } from "@shared/dto/errors/json-api.error";
 
 import { useToast } from "@/components/ui/use-toast";
 
-export type ApiResponseToast = {
+type ApiResponse<T = unknown> = {
+  headers: Headers;
   status: number;
-  body?: { errors: JSONAPIErrorOptions[] };
+  body: { errors?: JSONAPIErrorOptions[] } | T;
 };
 
 export const useApiResponseToast = () => {
   const { toast } = useToast();
-  const apiResponseToast = (
-    response: ApiResponseToast,
+
+  const apiResponseToast = <T>(
+    response: ApiResponse<T>,
     options: { successMessage: string; customErrorMessage?: string },
   ) => {
     if (response.status >= 200 && response.status < 300) {
       toast({ description: options.successMessage });
       return;
     }
+
     if (response.status >= 400 && response.status < 500) {
-      options.customErrorMessage
-        ? toast({
-            variant: "destructive",
-            description: options.customErrorMessage,
-          })
-        : response.body?.errors.forEach((error: JSONAPIErrorOptions) => {
+      if (options.customErrorMessage) {
+        toast({
+          variant: "destructive",
+          description: options.customErrorMessage,
+        });
+      } else if (
+        response.body &&
+        typeof response.body === "object" &&
+        "errors" in response.body
+      ) {
+        (response.body as { errors: JSONAPIErrorOptions[] }).errors?.forEach(
+          (error) => {
             toast({ description: error.title });
-          });
+          },
+        );
+      }
       return;
     }
+
     toast({
       variant: "destructive",
       description: "Something went wrong. Please try again.",
     });
   };
+
   return { apiResponseToast, toast };
 };
