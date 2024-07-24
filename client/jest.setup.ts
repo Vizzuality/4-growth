11dbb695b4695a649b4e34978c11c57e95f739d5
alexeh <alexurola@gmail.com>
2024-07-24 09:40:25 +0200

    globally mock ResizeObserver, add tests for contact-us form

--- client/jest.setup.ts
+++ client/jest.setup.ts
@@ -1,3 +1,8 @@
 // Globally import jest on test runs
 
 import "@testing-library/jest-dom";
+global.ResizeObserver = jest.fn().mockImplementation(() => ({
+  observe: jest.fn(),
+  unobserve: jest.fn(),
+  disconnect: jest.fn(),
+}));
