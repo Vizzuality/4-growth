04f3c5f163022c862388494e04028757be39e810
alexeh <alexurola@gmail.com>
2024-07-24 09:24:53 +0200

    plug api for contact form, add loading for send button

--- shared/contracts/index.ts
+++ shared/contracts/index.ts
@@ -3,10 +3,12 @@ import { initContract } from '@ts-rest/core';
 import { authContract } from './auth.contract';
 import { userContract } from './user.contract';
 import { customChartsContract } from './custom-charts.contract';
+import { contactContract } from './contact.contract';
 
 const c = initContract();
 
 export const router = c.router({
+  contact: contactContract,
   auth: authContract,
   user: userContract,
   userCharts: customChartsContract,
