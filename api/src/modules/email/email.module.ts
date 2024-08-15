795ed3ae28318ef67282f59924657553b2666d78
alexeh <alexurola@gmail.com>
2024-08-15 08:42:34 +0200

    mock email provider when node env is test

--- api/src/modules/email/email.module.ts
+++ api/src/modules/email/email.module.ts
@@ -1,14 +1,23 @@
 import { Module } from '@nestjs/common';
 import { NodemailerEmailService } from './nodemailer.email.service';
 import { IEmailServiceToken } from '@api/modules/email/email.service.interface';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MockEmailService } from '../../../test/utils/mocks/mock-email.service';
+
+export const emailServiceFactory = {
+  provide: IEmailServiceToken,
+  useFactory: (configService: ConfigService) => {
+    const env = configService.get<string>('NODE_ENV');
+    return env === 'test'
+      ? new MockEmailService()
+      : new NodemailerEmailService();
+  },
+  inject: [ConfigService],
+};
 
 @Module({
-  providers: [
-    {
-      provide: IEmailServiceToken,
-      useClass: NodemailerEmailService,
-    },
-  ],
+  imports: [ConfigModule],
+  providers: [emailServiceFactory],
   exports: [IEmailServiceToken],
 })
 export class EmailModule {}
--- api/test/utils/mocks/mock-email.service.ts
+++ api/test/utils/mocks/mock-email.service.ts
@@ -4,11 +4,23 @@ import {
 } from '@api/modules/email/email.service.interface';
 import { Logger } from '@nestjs/common';
 
+/**
+ * @description: Mock implementation of the Email Service Interface. The conditional logic is to allow for:
+ * 1. Jest to mock the sendMail method, and to check if the method is called in API tests
+ * 2. For e2e tests, we build the API with NODE_ENV=test, but in production mode, so jest is not available
+ */
+
 export class MockEmailService implements IEmailServiceInterface {
   logger: Logger = new Logger(MockEmailService.name);
 
-  sendMail = jest.fn(async (sendMailDTO: SendMailDTO): Promise<void> => {
-    this.logger.log('Mock Email sent');
-    return Promise.resolve();
-  });
+  sendMail =
+    typeof jest !== 'undefined'
+      ? jest.fn(async (sendMailDTO: SendMailDTO): Promise<void> => {
+          this.logger.log('Mock Email sent');
+          return Promise.resolve();
+        })
+      : async (sendMailDTO: SendMailDTO): Promise<void> => {
+          this.logger.log('Mock Email sent');
+          return Promise.resolve();
+        };
 }
