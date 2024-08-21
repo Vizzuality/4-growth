07212d90a485b95ca7a814f70a1489f3c27f3716
alexeh <alexurola@gmail.com>
2024-08-21 10:12:17 +0200

    handle odata database creation

--- scripts/sql/create-odata-db.sql
+++ scripts/sql/create-odata-db.sql
@@ -1,5 +1,8 @@
 
 -- This script creates a database in the local postgresql instance for the OData server which is used for development and testing purposes.
--- It expects to be created when you launch the odata server, and it automatically executed when you run the database from the docker-compose file.
+-- Since CREATE DATABASE is not allowed in functions or transaction block, we drop the database if exists to avoid errors. We will also add a script to populate
+-- the database with the data.
+
+DROP DATABASE IF EXISTS "4growth-odata";
 
 CREATE DATABASE "4growth-odata" OWNER "4growth";
