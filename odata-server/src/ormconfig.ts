3b4c1771776e60ba7f0393fea3e997e6ef876e2a
alexeh <alexurola@gmail.com>
2024-08-21 08:39:45 +0200

    create scripts to run odata server

--- odata-server/src/ormconfig.ts
+++ odata-server/src/ormconfig.ts
@@ -3,7 +3,7 @@ import { SurveyResponse } from './entities/survey-response.entity';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.ODATA_DB_HOST || 'localhost',
+  host: 'localhost',
   port: 5432,
   username: '4growth',
   password: '4growth',
