a070d320920cc087b0bf6b9045935b55ad5da191
atrincas <atrincas@gmail.com>
2024-09-09 14:47:31 +0200

    Removed unused jest setup file

--- client/jest.setup.ts
+++
@@ -1,8 +0,0 @@
-// Globally import jest on test runs
-
-import "@testing-library/jest-dom";
-global.ResizeObserver = jest.fn().mockImplementation(() => ({
-  observe: jest.fn(),
-  unobserve: jest.fn(),
-  disconnect: jest.fn(),
-}));
