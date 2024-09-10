b30e1a6c3db68863734ac8a2217cccc229fae9c1
atrincas <atrincas@gmail.com>
2024-09-10 16:22:34 +0200

    Added some styling to sidebar accordions

--- client/src/containers/sidebar/index.tsx
+++ client/src/containers/sidebar/index.tsx
@@ -9,74 +9,78 @@ import { cn } from "@/lib/utils";
 import Header from "@/containers/header";
 
 import {
-  Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
 import { Button } from "@/components/ui/button";
+import SidebarAccordion from "@/containers/sidebar/sidebar-accordion";
 
 const Sidebar: FC = () => {
-  const currentPage = usePathname().startsWith("/explore/sandbox/")
+  const currentPage = usePathname().startsWith("/sandbox")
     ? "sandbox"
     : "explore";
   let AccordionComponent = null;
 
   if (currentPage === "explore") {
     AccordionComponent = (
-      <Accordion type="single" collapsible className="w-full">
-        <AccordionItem value="item-1">
+      <SidebarAccordion
+        key="sidebar-accordion-explore"
+        defaultValue={["explore-sections"]}
+      >
+        <AccordionItem value="explore-filters">
           <AccordionTrigger>Filters</AccordionTrigger>
           <AccordionContent>filters here...</AccordionContent>
         </AccordionItem>
-        <AccordionItem value="item-2">
+        <AccordionItem value="explore-sections">
           <AccordionTrigger>Sections</AccordionTrigger>
-          <AccordionContent>sections here...</AccordionContent>
+          <AccordionContent></AccordionContent>
         </AccordionItem>
-      </Accordion>
+      </SidebarAccordion>
     );
   }
 
   if (currentPage === "sandbox") {
     AccordionComponent = (
-      <Accordion type="single" collapsible className="w-full">
-        <AccordionItem value="item-1">
+      <SidebarAccordion
+        key="sidebar-accordion-sandbox"
+        defaultValue={["sandbox-settings", "sandbox-filters"]}
+      >
+        <AccordionItem value="sandbox-settings">
           <AccordionTrigger>Settings</AccordionTrigger>
           <AccordionContent>settings here...</AccordionContent>
         </AccordionItem>
-        <AccordionItem value="item-2">
+        <AccordionItem value="sandbox-filters">
           <AccordionTrigger>Filters</AccordionTrigger>
           <AccordionContent>filters here...</AccordionContent>
         </AccordionItem>
-      </Accordion>
+      </SidebarAccordion>
     );
   }
   return (
     <>
       <Header />
       <div className="flex rounded-2xl bg-primary">
-        <Link className="flex-1" href="/explore">
-          <Button
-            variant="transparent"
-            className={cn(
-              "w-full",
-              currentPage === "explore" && "bg-white text-primary",
-            )}
-          >
-            Explore
-          </Button>
-        </Link>
-        <Link className="flex-1" href="/explore/sandbox/1">
-          <Button
-            variant="transparent"
-            className={cn(
-              "w-full",
-              currentPage === "sandbox" && "bg-white text-primary",
-            )}
-          >
-            Sandbox
-          </Button>
-        </Link>
+        <Button
+          variant="transparent"
+          className={cn(
+            "w-full",
+            currentPage === "explore" && "bg-white text-primary",
+          )}
+          asChild
+        >
+          <Link href="/explore">Explore</Link>
+        </Button>
+        <Button
+          variant="transparent"
+          className={cn(
+            "w-full",
+            currentPage === "sandbox" && "bg-white text-primary",
+          )}
+          asChild
+        >
+          <Link href="/sandbox">Sandbox</Link>
+        </Button>
       </div>
       {AccordionComponent}
     </>
