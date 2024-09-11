cd0db769f2625de7adad1aaa2f07532989857814
atrincas <atrincas@gmail.com>
2024-09-11 12:24:19 +0200

    Fixed missing href value

--- client/src/containers/explore/section/tile-menu/index.tsx
+++ client/src/containers/explore/section/tile-menu/index.tsx
@@ -12,7 +12,7 @@ const TileMenu: FC<TileMenuProps> = ({ items }) => {
   return (
     <div className="grid grid-cols-3 gap-0.5">
       {items.map((i) => (
-        <Link key={`tile-menu-${i.href}`} href="">
+        <Link key={`tile-menu-${i.href}`} href={i.href}>
           <Card className="bg-primary p-6 hover:bg-secondary">
             <h3 className="mb-6 font-semibold">{i.title}</h3>
             <p className="text-muted-foreground">{i.description}</p>
