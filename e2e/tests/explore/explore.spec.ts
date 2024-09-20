9cd44f3750d5cb4a36efb5362a80ea90539e5b6b
atrincas <atrincas@gmail.com>
2024-09-20 11:20:26 +0200

    Added explore test

--- e2e/tests/explore/explore.spec.ts
+++ e2e/tests/explore/explore.spec.ts
@@ -1,4 +1,5 @@
 import { test, Page, expect } from '@playwright/test'
+import { Section } from '@shared/dto/sections/section.entity'
 import { E2eTestManager } from '@shared/lib/e2e-test-manager'
 
 test.describe.configure({ mode: 'serial' })
@@ -6,12 +7,30 @@ test.describe.configure({ mode: 'serial' })
 test.describe('Explore E2E', () => {
   let testManager: E2eTestManager
   let page: Page
+  let sections: Section[]
 
   test.beforeAll(async ({ browser }) => {
     page = await browser.newPage()
     testManager = await E2eTestManager.load(page)
   })
 
+  test.beforeEach(async () => {
+    const entityMocks = testManager.mocks()
+    sections = [
+      await entityMocks.createSection({
+        slug: 'section-1',
+        name: 'Section 1',
+        description: ':)',
+        order: 1
+      }),
+      await entityMocks.createSection({
+        slug: 'section-2',
+        name: 'Section 2',
+        description: ':)',
+        order: 2
+      })
+    ]
+  })
   test.afterEach(async () => {
     await testManager.clearDatabase()
   })
@@ -20,25 +39,9 @@ test.describe('Explore E2E', () => {
     await testManager.close()
   })
 
-  // TODO: Fix test
-  // test('List all sections in the sidebar and main body', async () => {
-  //   const entityMocks = testManager.mocks()
-  //   const sections = [
-  //     await entityMocks.createSection({
-  //       slug: 'section-1',
-  //       name: 'Section 1',
-  //       description: ':)',
-  //       order: 1
-  //     }),
-  //     await entityMocks.createSection({
-  //       slug: 'section-2',
-  //       name: 'Section 2',
-  //       description: ':)',
-  //       order: 2
-  //     })
-  //   ]
-  //   await page.goto('/explore')
-  //   expect(await page.getByTestId('section-container').count()).toBe(sections.length)
-  //   expect(await page.getByTestId('section-link').count()).toBe(sections.length)
-  // })
+  test('List all sections in the sidebar and main body', async () => {
+    await page.goto('/explore')
+    expect(await page.getByTestId('section-container').count()).toBe(sections.length)
+    expect(await page.getByTestId('section-link').count()).toBe(sections.length)
+  })
 })
