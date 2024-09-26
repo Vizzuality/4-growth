596b3d4583421e6d2aaefba399b76d50a5e500b1
Alejandro Peralta <alejandro.peralta@vizzuality.com>
2024-09-26 17:33:41 +0200

    feat(api): Add SearchDataWidgetParams in order to allow users to filter the data returned allong with the sections' widgets

--- shared/contracts/sections.contract.ts
+++ shared/contracts/sections.contract.ts
@@ -2,6 +2,7 @@ import { JSONAPIError } from '@shared/dto/errors/json-api.error';
 import { ApiPaginationResponse } from '@shared/dto/global/api-response.dto';
 import { SectionWithDataWidget } from '@shared/dto/sections/section.entity';
 import { generateEntityQuerySchema } from '@shared/schemas/query-param.schema';
+import { SearchWidgetDataParamsSchema } from '@shared/schemas/search-widget-data-params.schema';
 import { initContract } from '@ts-rest/core';
 
 const contract = initContract();
@@ -9,7 +10,9 @@ export const sectionContract = contract.router({
   searchSections: {
     method: 'GET',
     path: '/sections',
-    query: generateEntityQuerySchema(SectionWithDataWidget),
+    query: generateEntityQuerySchema(SectionWithDataWidget).merge(
+      SearchWidgetDataParamsSchema,
+    ),
     responses: {
       200: contract.type<ApiPaginationResponse<SectionWithDataWidget>>(),
       400: contract.type<JSONAPIError>(),
