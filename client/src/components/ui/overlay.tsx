748830a9e9fb00a9c386c002a1e743cf9a3b8bfa
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2024-10-02 13:30:43 +0200

    updates about page images

--- client/src/components/ui/overlay.tsx
+++ client/src/components/ui/overlay.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
 
 export const Overlay: FC = () => (
-  <div className="absolute left-0 top-0 z-40 h-full w-full bg-navy-900/75" />
+  <div className="fixed left-0 top-0 z-40 h-full w-full bg-navy-900/75" />
 );
