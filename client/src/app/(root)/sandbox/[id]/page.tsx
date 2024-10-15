efe858f18b9e84d7a4ec790ee6c94f2cde91213f
atrincas <atrincas@gmail.com>
2024-10-15 15:50:59 +0200

    WIP

--- client/src/app/(root)/sandbox/[id]/page.tsx
+++ client/src/app/(root)/sandbox/[id]/page.tsx
@@ -1,5 +1,8 @@
 import Sandbox from "@/containers/sandbox";
 
+// Get widget by id
+// Get all filters
+// Get all widgets (id + indicator)
 export default async function SandboxPage() {
   return <Sandbox />;
 }
