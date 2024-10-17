eb1c1883254309b520e2ac0ab1159e2e9df43569
atrincas <atrincas@gmail.com>
2024-10-17 16:26:01 +0200

    Refactor

--- shared/dto/widgets/widget.utils.ts
+++ shared/dto/widgets/widget.utils.ts
@@ -1,5 +1,7 @@
+import { BaseWidgetWithData } from '@shared/dto/widgets/base-widget-data.interface';
 import {
   VALID_WIDGET_VISUALIZATIONS,
+  WIDGET_VISUALIZATIONS,
   WidgetVisualizationsType,
 } from '@shared/dto/widgets/widget-visualizations.constants';
 
@@ -20,7 +22,27 @@ const isValidDefaultVisualization = (
   );
 };
 
+const getSupportedVisualizations = (widget: BaseWidgetWithData) => ({
+  supportsChart: widget.visualisations.some((v) =>
+    (
+      [
+        WIDGET_VISUALIZATIONS.AREA_GRAPH,
+        WIDGET_VISUALIZATIONS.HORIZONTAL_BAR_CHART,
+        WIDGET_VISUALIZATIONS.PIE_CHART,
+      ] as WidgetVisualizationsType[]
+    ).includes(v),
+  ),
+  supportsSingleValue: widget.visualisations.includes(
+    WIDGET_VISUALIZATIONS.SINGLE_VALUE,
+  ),
+  supportsMap: widget.visualisations.includes(WIDGET_VISUALIZATIONS.MAP),
+  supportsNavigation: widget.visualisations.includes(
+    WIDGET_VISUALIZATIONS.NAVIGATION,
+  ),
+});
+
 export const WidgetUtils = {
   isValidVisualization,
   isValidDefaultVisualization,
+  getSupportedVisualizations,
 };
