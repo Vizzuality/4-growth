10849a7092b553374e111a2f55782048a08f5237
atrincas <atrincas@gmail.com>
2024-10-18 16:21:29 +0200

    Removed comments

--- client/src/app/(root)/sandbox/[id]/page.tsx
+++ client/src/app/(root)/sandbox/[id]/page.tsx
@@ -1,8 +1,5 @@
 import Sandbox from "@/containers/sandbox";
 
-// Get widget by id
-// Get all filters
-// Get all widgets (id + indicator)
 export default async function SandboxPage() {
   return <Sandbox />;
 }
