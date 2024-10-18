1aa588dfbe8db564312e9aec8ae526cfc150e031
alexeh <alexurola@gmail.com>
2024-10-18 08:32:03 +0200

    widget visualisation filter optional, remove visualisation filter for single widget

--- shared/schemas/widget-visualisation-filters.schema.ts
+++ shared/schemas/widget-visualisation-filters.schema.ts
@@ -5,9 +5,9 @@ import {
 } from '@shared/dto/widgets/widget-visualizations.constants';
 
 export const WidgetVisualizationFilterSchema = z.object({
-  visualisations: z.array(
-    z.enum(VALID_WIDGET_VISUALIZATIONS as [WidgetVisualizationsType]),
-  ),
+  visualisations: z
+    .array(z.enum(VALID_WIDGET_VISUALIZATIONS as [WidgetVisualizationsType]))
+    .optional(),
 });
 
 export type WidgetVisualisationFilters = z.infer<
