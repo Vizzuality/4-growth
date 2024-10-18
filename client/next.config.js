34cffcdc28457f68cdf86da94fed4f334a5f301c
atrincas <atrincas@gmail.com>
2024-10-18 11:25:08 +0200

    Ignore typeorm warnings during build time

--- client/next.config.js
+++ client/next.config.js
@@ -6,6 +6,19 @@ const jiti = createJiti(fileURLToPath(import.meta.url));
 jiti("./src/env");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config) {
+    const warning = [
+      ...(config.ignoreWarnings || []),
+      { module: /typeorm/ },
+      {
+        module: /app-root-path/,
+        message: /the request of a dependency is an expression/,
+      },
+    ];
+    config.ignoreWarnings = warning;
+    return config;
+  },
+};
 
 export default nextConfig;
