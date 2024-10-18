f8945306337da97cfc421fbb690cb923fce5b914
atrincas <atrincas@gmail.com>
2024-10-18 12:23:47 +0200

    Make Typeorm ingore list more explicit

--- client/next.config.js
+++ client/next.config.js
@@ -10,7 +10,22 @@ const nextConfig = {
   webpack(config) {
     const warning = [
       ...(config.ignoreWarnings || []),
-      { module: /typeorm/ },
+      {
+        module: /typeorm/,
+        message: /the request of a dependency is an expression/,
+      },
+      {
+        module: /typeorm/,
+        message: /Can't resolve 'react-native-sqlite-storage'/,
+      },
+      {
+        module: /typeorm/,
+        message: /Can't resolve '@sap\/hana-client\/extension\/Stream'/,
+      },
+      {
+        module: /typeorm/,
+        message: /Can't resolve 'mysql'/,
+      },
       {
         module: /app-root-path/,
         message: /the request of a dependency is an expression/,
