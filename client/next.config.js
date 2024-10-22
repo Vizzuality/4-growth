a5d52c9ad3e4260b7f4deb2d4015cc7b4cfe8f2f
alexeh <alexurola@gmail.com>
2024-10-22 09:37:58 +0200

    test remove node:url dep in nextconfig

--- client/next.config.js
+++ client/next.config.js
@@ -1,9 +1,9 @@
-import { fileURLToPath } from "node:url";
+// import { fileURLToPath } from "node:url";
+//
+// import createJiti from "jiti";
+// const jiti = createJiti(fileURLToPath(import.meta.url));
 
-import createJiti from "jiti";
-const jiti = createJiti(fileURLToPath(import.meta.url));
-
-jiti("./src/env");
+// jiti("./src/env");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
