36088fe39e888e12e2b48235c046224ac9c9c554
alexeh <alexurola@gmail.com>
2024-10-22 09:22:51 +0200

    test experimental config

--- client/next.config.js
+++ client/next.config.js
@@ -7,6 +7,9 @@ jiti("./src/env");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ["typeorm"],
+  },
   webpack(config) {
     const warning = [
       ...(config.ignoreWarnings || []),
