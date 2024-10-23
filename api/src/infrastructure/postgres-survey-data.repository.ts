1922918bb7e877a099a42dc6f10c6198727627ed
Alejandro Peralta <alejandro.peralta@vizzuality.com>
2024-10-23 19:08:34 +0200

    refactor(api): Refactor survey-data.repository.ts and add a method to allow devs to retrieve data fora  single widget

--- api/src/infrastructure/postgres-survey-data.repository.ts
+++ api/src/infrastructure/postgres-survey-data.repository.ts
@@ -13,6 +13,8 @@ import {
 import { WidgetUtils } from '@shared/dto/widgets/widget.utils';
 
 export class PostgresSurveyDataRepository implements ISurveyDataRepository {
+  private readonly answersTable: string = 'survey_answers';
+
   public constructor(
     private readonly logger: Logger,
     @Inject(SQLAdapter) private readonly sqlAdapter: SQLAdapter,
@@ -23,9 +25,7 @@ export class PostgresSurveyDataRepository implements ISurveyDataRepository {
     sections: SectionWithDataWidget[],
     filters?: WidgetDataFilters,
   ): Promise<SectionWithDataWidget[]> {
-    const answersTable: string = 'survey_answers';
     let filterClause: string;
-
     if (filters !== undefined) {
       filterClause = this.sqlAdapter.generateSqlFromWidgetDataFilters(filters);
     }
@@ -37,7 +37,7 @@ export class PostgresSurveyDataRepository implements ISurveyDataRepository {
       for (let widgetIdx = 0; widgetIdx < baseWidgets.length; widgetIdx++) {
         const widget = baseWidgets[widgetIdx];
         widgetDataPromises.push(
-          this.appendBaseWidgetData(answersTable, filterClause, widget),
+          this.appendBaseWidgetData(widget, filterClause),
         );
       }
     }
@@ -46,16 +46,28 @@ export class PostgresSurveyDataRepository implements ISurveyDataRepository {
     return sections;
   }
 
-  private async appendBaseWidgetData(
-    answersTable: string,
-    filterClause: string,
+  public async addSurveyDataToBaseWidget(
     widget: BaseWidgetWithData,
+    filters?: WidgetDataFilters,
+  ): Promise<BaseWidgetWithData> {
+    let filterClause: string;
+    if (filters !== undefined) {
+      filterClause = this.sqlAdapter.generateSqlFromWidgetDataFilters(filters);
+    }
+
+    await this.appendBaseWidgetData(widget, filterClause);
+    return widget;
+  }
+
+  private async appendBaseWidgetData(
+    widget: BaseWidgetWithData,
+    filterClause: string,
   ): Promise<void> {
     const { indicator } = widget;
     // Edge cases here. Total surveys and total countries.
     if (indicator === 'total-surveys') {
-      const filteredCount = `SELECT COUNT(count)::integer as count FROM (SELECT COUNT(DISTINCT survey_id) FROM ${answersTable} ${filterClause} GROUP BY survey_id) AS survey_count`;
-      const totalCount = `SELECT COUNT(count)::integer as count FROM (SELECT COUNT(DISTINCT survey_id) FROM ${answersTable} GROUP BY survey_id) AS survey_count`;
+      const filteredCount = `SELECT COUNT(count)::integer as count FROM (SELECT COUNT(DISTINCT survey_id) FROM ${this.answersTable} ${filterClause} GROUP BY survey_id) AS survey_count`;
+      const totalCount = `SELECT COUNT(count)::integer as count FROM (SELECT COUNT(DISTINCT survey_id) FROM ${this.answersTable} GROUP BY survey_id) AS survey_count`;
       const [[{ count: value }], [{ count: total }]] = await Promise.all([
         this.dataSource.query(filteredCount),
         this.dataSource.query(totalCount),
@@ -64,8 +76,8 @@ export class PostgresSurveyDataRepository implements ISurveyDataRepository {
       return;
     }
     if (indicator === 'total-countries') {
-      const filteredCount = `SELECT COUNT(DISTINCT country_code)::integer as "count" FROM ${answersTable} ${filterClause}`;
-      const totalCount = `SELECT COUNT(DISTINCT country_code)::integer as "count" FROM ${answersTable};`;
+      const filteredCount = `SELECT COUNT(DISTINCT country_code)::integer as "count" FROM ${this.answersTable} ${filterClause}`;
+      const totalCount = `SELECT COUNT(DISTINCT country_code)::integer as "count" FROM ${this.answersTable};`;
       const [[{ count: value }], [{ count: total }]] = await Promise.all([
         this.dataSource.query(filteredCount),
         this.dataSource.query(totalCount),
@@ -74,7 +86,7 @@ export class PostgresSurveyDataRepository implements ISurveyDataRepository {
       return;
     }
 
-    const totalsSql = `SELECT answer as "key", count(answer)::integer as "count", SUM(COUNT(answer)) OVER ()::integer AS total FROM ${answersTable} ${this.sqlAdapter.appendExpressionToFilterClause(filterClause, `question_indicator = '${indicator}'`)} GROUP BY answer`;
+    const totalsSql = `SELECT answer as "key", count(answer)::integer as "count", SUM(COUNT(answer)) OVER ()::integer AS total FROM ${this.answersTable} ${this.sqlAdapter.appendExpressionToFilterClause(filterClause, `question_indicator = '${indicator}'`)} GROUP BY answer`;
     const totalsResult: { key: string; count: number; total: number }[] =
       await this.dataSource.query(totalsSql);
 
@@ -116,4 +128,102 @@ export class PostgresSurveyDataRepository implements ISurveyDataRepository {
 
     widget.data = widgetData;
   }
+
+  // public async addSurveyDataToSections(
+  //   sections: SectionWithDataWidget[],
+  //   filters?: WidgetDataFilters,
+  // ): Promise<SectionWithDataWidget[]> {
+  //   const answersTable: string = 'survey_answers';
+  //   let filterClause: string;
+
+  //   if (filters !== undefined) {
+  //     filterClause = this.sqlAdapter.generateSqlFromWidgetDataFilters(filters);
+  //   }
+
+  //   const widgetDataPromises = [];
+  //   for (let sectionIdx = 0; sectionIdx < sections.length; sectionIdx++) {
+  //     const section = sections[sectionIdx];
+  //     const baseWidgets = section.baseWidgets;
+  //     for (let widgetIdx = 0; widgetIdx < baseWidgets.length; widgetIdx++) {
+  //       const widget = baseWidgets[widgetIdx];
+  //       widgetDataPromises.push(
+  //         this.appendBaseWidgetData(answersTable, filterClause, widget),
+  //       );
+  //     }
+  //   }
+
+  //   await Promise.all(widgetDataPromises);
+  //   return sections;
+  // }
+
+  // private async appendBaseWidgetData(
+  //   answersTable: string,
+  //   filterClause: string,
+  //   widget: BaseWidgetWithData,
+  // ): Promise<void> {
+  //   const { indicator } = widget;
+  //   // Edge cases here. Total surveys and total countries.
+  //   if (indicator === 'total-surveys') {
+  //     const filteredCount = `SELECT COUNT(count)::integer as count FROM (SELECT COUNT(DISTINCT survey_id) FROM ${answersTable} ${filterClause} GROUP BY survey_id) AS survey_count`;
+  //     const totalCount = `SELECT COUNT(count)::integer as count FROM (SELECT COUNT(DISTINCT survey_id) FROM ${answersTable} GROUP BY survey_id) AS survey_count`;
+  //     const [[{ count: value }], [{ count: total }]] = await Promise.all([
+  //       this.dataSource.query(filteredCount),
+  //       this.dataSource.query(totalCount),
+  //     ]);
+  //     widget.data = { counter: { value, total } };
+  //     return;
+  //   }
+  //   if (indicator === 'total-countries') {
+  //     const filteredCount = `SELECT COUNT(DISTINCT country_code)::integer as "count" FROM ${answersTable} ${filterClause}`;
+  //     const totalCount = `SELECT COUNT(DISTINCT country_code)::integer as "count" FROM ${answersTable};`;
+  //     const [[{ count: value }], [{ count: total }]] = await Promise.all([
+  //       this.dataSource.query(filteredCount),
+  //       this.dataSource.query(totalCount),
+  //     ]);
+  //     widget.data = { counter: { value, total } };
+  //     return;
+  //   }
+
+  //   const totalsSql = `SELECT answer as "key", count(answer)::integer as "count", SUM(COUNT(answer)) OVER ()::integer AS total FROM ${answersTable} ${this.sqlAdapter.appendExpressionToFilterClause(filterClause, `question_indicator = '${indicator}'`)} GROUP BY answer`;
+  //   const totalsResult: { key: string; count: number; total: number }[] =
+  //     await this.dataSource.query(totalsSql);
+
+  //   if (totalsResult.length === 0) {
+  //     widget.data = {};
+  //     return;
+  //   }
+
+  //   const widgetData: WidgetData = {};
+  //   const {
+  //     supportsChart,
+  //     supportsSingleValue,
+  //     supportsMap,
+  //     supportsNavigation,
+  //   } = WidgetUtils.getSupportedVisualizations(widget);
+
+  //   if (supportsChart) {
+  //     const arr: WidgetChartData = [];
+
+  //     for (let rowIdx = 0; rowIdx < totalsResult.length; rowIdx++) {
+  //       const res = totalsResult[rowIdx];
+  //       arr.push({ label: res.key, value: res.count, total: res.total });
+  //     }
+
+  //     widgetData.chart = arr;
+  //   }
+
+  //   if (supportsSingleValue) {
+  //     // TODO: Add WidgetCounterData
+  //   }
+
+  //   if (supportsMap) {
+  //     // TODO: Add WidgetMapData
+  //   }
+
+  //   if (supportsNavigation) {
+  //     // TODO: Add WidgetNavigationData
+  //   }
+
+  //   widget.data = widgetData;
+  // }
 }
