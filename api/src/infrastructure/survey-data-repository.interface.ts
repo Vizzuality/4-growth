c450b1036049df0e30502a922632ab00fafc72f8
Alejandro Peralta <alejandro.peralta@vizzuality.com>
2024-10-23 19:08:34 +0200

    refactor(api): Refactor survey-data.repository.ts and add a method to allow devs to retrieve data fora  single widget

--- api/src/infrastructure/survey-data-repository.interface.ts
+++ api/src/infrastructure/survey-data-repository.interface.ts
@@ -1,8 +1,17 @@
-import { Section } from '@shared/dto/sections/section.entity';
+import {
+  Section,
+  SectionWithDataWidget,
+} from '@shared/dto/sections/section.entity';
+import { BaseWidgetWithData } from '@shared/dto/widgets/base-widget-data.interface';
+import { BaseWidget } from '@shared/dto/widgets/base-widget.entity';
 import { WidgetDataFilter } from '@shared/dto/widgets/widget-data-filter';
 export interface ISurveyDataRepository {
   addSurveyDataToSections(
     sections: Partial<Section>[],
     filters?: WidgetDataFilter[],
-  ): Promise<any>;
+  ): Promise<SectionWithDataWidget[]>;
+  addSurveyDataToBaseWidget(
+    widget: BaseWidget,
+    filters?: WidgetDataFilter[],
+  ): Promise<BaseWidgetWithData>;
 }
