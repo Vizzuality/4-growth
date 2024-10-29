d2f4e677b9a80f52cc65058f19890587b2627501
atrincas <atrincas@gmail.com>
2024-10-29 09:16:39 +0100

    Updated styles no-data component

---
+++ client/src/components/icons/no-data.tsx
@@ -0,0 +1,33 @@
+import { SVGProps } from "react";
+
+export const NoData = (props: SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      width="64"
+      height="64"
+      viewBox="0 0 64 64"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <g opacity="0.5">
+        <path
+          d="M52 38V31V22M52 22C49.136 22 45.387 22 43 22H39C38.2044 22 36 22 36 22C36 22 36 19.7956 36 19V15C36 12.6131 36 6 36 6M52 22C52 22 52 26.8347 52 30V51.9633M52 22L44.5 14.5L36 6M36 6C34.0322 6 29.2861 6 28 6H15H12V11.8899M36 6C33.9289 6 28.3191 6 27 6H22M20 40H24.25L30 40M20 48L37.7419 48M50.0323 58H15C13.344 58 12 58 12 58V20.1613"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M5.87207 5.87158L58.1289 58.1285"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </g>
+    </svg>
+  );
+};
+
+export default NoData;
--- client/tests/no-data.test.tsx
+++ client/tests/no-data.test.tsx
@@ -1,14 +1,18 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
 import NoData from "@/containers/no-data";
+
+vi.mock("@/components/icons/no-data", () => ({
+  default: () => <div data-testid="no-data-icon">No data icon</div>,
+}));
 
 describe("NoData", () => {
   it("renders the correct content", () => {
     render(<NoData />);
 
-    expect(screen.getByText("No data")).toBeInTheDocument();
+    expect(screen.getByTestId("no-data-icon")).toBeInTheDocument();
     expect(
-      screen.getByText("There was no data found for this visualization"),
+      screen.getByText("No data available for this item."),
     ).toBeInTheDocument();
   });
 });
