69f826a2e582cadbe4d5e1745cbc2c7524749ed4
Alejandro Peralta <alejandro.peralta@vizzuality.com>
2024-10-30 13:36:45 +0100

    feat(api): Add /health endpoint

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -3,19 +3,33 @@ import { AppService } from '@api/app.service';
 import { tsRestHandler, TsRestHandler } from '@ts-rest/nest';
 import { contactContract as c } from '@shared/contracts/contact.contract';
 import { Public } from '@api/decorators/is-public.decorator';
+import { ControllerResponse } from '@api/types/controller.type';
+import {
+  HealthCheck,
+  HealthCheckService,
+  TypeOrmHealthIndicator,
+} from '@nestjs/terminus';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly health: HealthCheckService,
+    private readonly db: TypeOrmHealthIndicator,
+    private readonly appService: AppService,
+  ) {}
 
   @Public()
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/health')
+  @HealthCheck({ noCache: true })
+  public checkHealth(): ControllerResponse {
+    return this.health.check([
+      async () => this.db.pingCheck('database', { timeout: 1500 }),
+    ]);
   }
+
   @Public()
   @TsRestHandler(c.contact)
-  async recoverPassword(): Promise<any> {
+  async recoverPassword(): Promise<ControllerResponse> {
     return tsRestHandler(c.contact, async ({ body }) => {
       await this.appService.contact(body);
       return { body: null, status: HttpStatus.CREATED };
--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -4,9 +4,6 @@ import { ContactForm, ContactMailer } from '@api/contact.mailer';
 @Injectable()
 export class AppService {
   constructor(private readonly contactMailer: ContactMailer) {}
-  getHello(): string {
-    return 'Hello World!';
-  }
 
   async contact(contactForm: ContactForm): Promise<void> {
     await this.contactMailer.sendContactMail(contactForm);
