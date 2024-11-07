e09cc65d49b424bdce1f881d26625690d3aa955c
Alejandro Peralta <alejandro.peralta@vizzuality.com>
2024-11-07 13:54:40 +0100

    chore(shared): Change custom-widget model and make widget not null

--- api/src/migrations/1729163940111-initial-tables-and-indexes.ts
+++ api/src/migrations/1729163940111-initial-tables-and-indexes.ts
@@ -20,7 +20,7 @@ export class InitialTablesAndIndexes1729163940111
       `CREATE INDEX "idx_section_widgets_section_order" ON "base_widgets" ("section_order") `,
     );
     await queryRunner.query(
-      `CREATE TABLE "custom_widgets" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "default_visualization" "public"."custom_widgets_default_visualization_enum" NOT NULL, "filters" jsonb NOT NULL, "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "created_at" TIMESTAMP NOT NULL DEFAULT now(), "user_id" uuid, "widget_indicator" character varying, CONSTRAINT "PK_a5d2205ca142399bde7d3f2b1cb" PRIMARY KEY ("id"))`,
+      `CREATE TABLE "custom_widgets" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "default_visualization" "public"."custom_widgets_default_visualization_enum" NOT NULL, "filters" jsonb NOT NULL, "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "created_at" TIMESTAMP NOT NULL DEFAULT now(), "user_id" uuid, "widget_indicator" character varying NOT NULL, CONSTRAINT "PK_a5d2205ca142399bde7d3f2b1cb" PRIMARY KEY ("id"))`,
     );
     await queryRunner.query(
       `CREATE TABLE "users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "email" character varying NOT NULL, "password" character varying NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`,
