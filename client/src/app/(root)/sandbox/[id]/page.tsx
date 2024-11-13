a6f1ce90a7a21ff96b2c10b0c21168eac207906f
atrincas <atrincas@gmail.com>
2024-11-13 14:36:25 +0100

    Added redirect when user is unauthorized to visit custom widget page

--- client/src/app/(root)/sandbox/[id]/page.tsx
+++ client/src/app/(root)/sandbox/[id]/page.tsx
@@ -8,6 +8,25 @@ import { auth } from "@/app/auth/api/[...nextauth]/config";
 import Sandbox from "@/containers/sandbox/user-sandbox";
 
 import { getAuthHeader } from "@/utils/auth-header";
+import { redirect } from "next/navigation";
+import { Session } from "next-auth";
+
+async function fetchCustomWidget(id: string, session: Session | null) {
+  try {
+    const response = await client.users.findCustomWidget.query({
+      params: { id: Number(id), userId: session?.user?.id as string },
+      extraHeaders: {
+        ...getAuthHeader(session?.accessToken as string),
+      },
+      ...QUERY_OPTIONS,
+    });
+
+    return response;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
 
 export default async function SandboxPage({
   params,
@@ -15,19 +34,19 @@ export default async function SandboxPage({
   params: Promise<{ id: string }>;
 }) {
   const id = (await params).id;
-  const queryClient = new QueryClient();
   const session = await auth();
 
+  const response = await fetchCustomWidget(id, session);
+
+  // TODO: Add proper redirect when API returns correct http codes (in another PR)
+  if (response?.status !== 200) {
+    redirect("/sandbox");
+  }
+
+  const queryClient = new QueryClient();
   await queryClient.prefetchQuery({
     queryKey: queryKeys.users.userChart(id).queryKey,
-    queryFn: async () =>
-      client.users.findCustomWidget.query({
-        params: { id: Number(id), userId: session?.user?.id as string },
-        extraHeaders: {
-          ...getAuthHeader(session?.accessToken as string),
-        },
-        ...QUERY_OPTIONS,
-      }),
+    queryFn: async () => response,
   });
 
   return (
