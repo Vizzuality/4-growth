8e5bccf440ef2882c46ec0dc70de72a2a571306f
atrincas <atrincas@gmail.com>
2024-11-25 11:07:38 +0100

    Show message when indicator fails or has no data

--- client/src/containers/sandbox/index.tsx
+++ client/src/containers/sandbox/index.tsx
@@ -8,13 +8,63 @@ import NoData from "@/containers/no-data";
 import Widget from "@/containers/widget";
 import CreateWidgetMenu from "@/containers/widget/create-widget";
 
+import CloudOff from "@/components/icons/cloud-off";
 import MenuPointer from "@/components/icons/menu-pointer";
 import { Card } from "@/components/ui/card";
 
 const Sandbox: FC = () => {
-  const { breakdown, indicator, visualization, setVisualization, widget } =
-    useSandboxWidget();
+  const {
+    breakdown,
+    indicator,
+    visualization,
+    setVisualization,
+    getWidgetQuery: { data: widget, error, isFetched },
+  } = useSandboxWidget();
   const { filters } = useFilters();
+  let Comp: JSX.Element | null = null;
+
+  if (isFetched && !widget && !error) {
+    Comp = (
+      <NoData
+        icon={<MenuPointer />}
+        description="Select an indicator to visualize its data."
+        className="m-6 gap-6"
+      />
+    );
+  }
+
+  if (widget) {
+    Comp = (
+      <Widget
+        breakdown={breakdown || undefined}
+        indicator={widget.indicator}
+        question={widget.question}
+        visualization={visualization || widget.defaultVisualization}
+        data={widget.data}
+        menu={
+          <CreateWidgetMenu
+            indicator={indicator}
+            visualization={visualization}
+            filters={filters}
+          />
+        }
+        className="col-span-1 last:odd:col-span-2"
+        config={{ menu: { className: "flex flex-col py-4" } }}
+      />
+    );
+  }
+
+  if (error) {
+    Comp =
+      error.status === 404 ? (
+        <NoData />
+      ) : (
+        <NoData
+          icon={<CloudOff />}
+          description="Something went wrong, please check back soon."
+        />
+      );
+  }
 
   useEffect(() => {
     if (!widget) return;
@@ -24,34 +74,7 @@ const Sandbox: FC = () => {
     }
   }, [widget, visualization]);
 
-  return (
-    <Card className="p-0">
-      {widget ? (
-        <Widget
-          breakdown={breakdown || undefined}
-          indicator={widget.indicator}
-          question={widget.question}
-          visualization={visualization || widget.defaultVisualization}
-          data={widget.data}
-          menu={
-            <CreateWidgetMenu
-              indicator={indicator}
-              visualization={visualization}
-              filters={filters}
-            />
-          }
-          className="col-span-1 last:odd:col-span-2"
-          config={{ menu: { className: "flex flex-col py-4" } }}
-        />
-      ) : (
-        <NoData
-          icon={<MenuPointer />}
-          description="Select an indicator to visualize its data."
-          className="m-6 gap-6"
-        />
-      )}
-    </Card>
-  );
+  return <Card className="p-0">{Comp}</Card>;
 };
 
 export default Sandbox;
