f14e105d7da88bd5ea00b3004873e5fa8bba461e
atrincas <atrincas@gmail.com>
2024-12-02 09:21:15 +0100

    Removed unnecessary div

--- client/src/app/error.tsx
+++ client/src/app/error.tsx
@@ -6,12 +6,10 @@ import CloudOff from "@/components/icons/cloud-off";
 
 export default function ErrorPage() {
   return (
-    <div className="h-full min-h-screen">
-      <NoData
-        className="h-full"
-        icon={<CloudOff />}
-        description="Something went wrong, please check back soon."
-      />
-    </div>
+    <NoData
+      className="h-full"
+      icon={<CloudOff />}
+      description="Something went wrong, please check back soon."
+    />
   );
 }
