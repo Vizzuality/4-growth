afa30cd131f129b1e4d1942e36b7837e15fbf3e9
atrincas <atrincas@gmail.com>
2025-06-10 12:12:58 +0200

    fix order useMemo hook for area-chart

--- client/src/containers/widget/area-chart/index.tsx
+++ client/src/containers/widget/area-chart/index.tsx
@@ -3,6 +3,7 @@ import { FC, useMemo } from "react";
 import { WidgetChartData } from "@shared/dto/widgets/base-widget-data.interface";
 
 import { cn } from "@/lib/utils";
+
 import NoData from "@/containers/no-data";
 
 interface AreaChartProps {
@@ -11,7 +12,7 @@ interface AreaChartProps {
 }
 
 const getAreaChartCategories = (
-  data: WidgetChartData,
+  data?: WidgetChartData,
 ): Record<"yes" | "no" | "dontKnow", WidgetChartData[number] | undefined> => {
   const categories: Record<
     "yes" | "no" | "dontKnow",
@@ -21,6 +22,8 @@ const getAreaChartCategories = (
     no: undefined,
     dontKnow: undefined,
   };
+
+  if (!data) return categories;
 
   data.forEach((item) => {
     if (item.label === "Yes") categories.yes = item;
@@ -32,6 +35,8 @@ const getAreaChartCategories = (
 };
 
 const AreaChart: FC<AreaChartProps> = ({ indicator, data }) => {
+  const categories = useMemo(() => getAreaChartCategories(data), [data]);
+
   if (!data || data.length === 0 || data.length > 3) {
     console.error(
       `AreaChart (${indicator}): Invalid data format. The chart requires 1-3 data points, but received ${data?.length || 0} points.`,
@@ -39,7 +44,6 @@ const AreaChart: FC<AreaChartProps> = ({ indicator, data }) => {
     return <NoData />;
   }
 
-  const categories = useMemo(() => getAreaChartCategories(data), [data]);
   return (
     <div className="relative flex h-full items-end">
       <div className="absolute left-6 top-6 z-10 h-[calc(100%_-_3rem)] w-[calc(100%_-_3rem)]">
