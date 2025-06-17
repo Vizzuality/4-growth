a575683602eeb58d59a5256c905b2056de7af82b
atrincas <atrincas@gmail.com>
2025-06-17 08:44:42 +0200

    add scenarios header in sidebar with info-button

--- client/src/containers/sidebar/projections-sidebar/explore-sidebar/index.tsx
+++ client/src/containers/sidebar/projections-sidebar/explore-sidebar/index.tsx
@@ -1,7 +1,38 @@
 import { FC } from "react";
 
+import ScenariosInfo from "@/containers/scenarios-info";
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import InfoButton from "@/components/ui/info-button";
+
 const ExploreSidebar: FC = () => {
-  return <div>Projections Explore Sidebar</div>;
+  return (
+    <Accordion
+      key="sidebar-accordion-explore"
+      type="multiple"
+      className="w-full overflow-y-auto"
+      defaultValue={["scenario"]}
+    >
+      <AccordionItem value="scenario">
+        <AccordionTrigger>
+          <div className="mr-1 flex w-full items-center justify-between">
+            <span>Scenarios</span>
+            <div className="inline-flex" onClick={(e) => e.stopPropagation()}>
+              <InfoButton title="Scenario">
+                <ScenariosInfo />
+              </InfoButton>
+            </div>
+          </div>
+        </AccordionTrigger>
+        <AccordionContent className="py-3.5"></AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  );
 };
 
 export default ExploreSidebar;
