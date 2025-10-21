c0ca843681d7428c13170ce6307da7d7d6e23f7b
atrincas <atrincas@gmail.com>
2025-10-21 15:35:10 +0200

    fe: fix overflow about page

--- client/src/app/about/page.tsx
+++ client/src/app/about/page.tsx
@@ -1,5 +1,9 @@
 import About from "@/containers/about";
 
 export default function AboutPage() {
-  return <About />;
+  return (
+    <div className="h-full overflow-y-auto">
+      <About />
+    </div>
+  );
 }
