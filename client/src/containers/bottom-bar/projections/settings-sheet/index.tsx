b13b08a1003d11d72097f758a96a488578dafd05
atrincas <atrincas@gmail.com>
2025-10-30 13:59:44 +0100

    fe: fix ui projections mobile sheets

--- client/src/containers/bottom-bar/projections/settings-sheet/index.tsx
+++ client/src/containers/bottom-bar/projections/settings-sheet/index.tsx
@@ -7,6 +7,7 @@ import { Button } from "@/components/ui/button";
 import {
   Sheet,
   SheetContent,
+  SheetDescription,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
@@ -18,9 +19,13 @@ const SettingsSheet: FC = () => {
       <SheetTrigger asChild>
         <Button className="w-full">Settings</Button>
       </SheetTrigger>
-      <SheetContent className="h-full w-screen bg-navy-900" side="bottom">
-        <SheetHeader>
-          <SheetTitle>Settings</SheetTitle>
+      <SheetContent
+        className="h-full max-h-[80%] w-screen rounded-t-2xl border-t-navy-900 bg-navy-900 px-0 pb-0"
+        side="bottom"
+      >
+        <SheetHeader className="mb-6">
+          <SheetTitle className="px-4 text-left text-base">Settings</SheetTitle>
+          <SheetDescription className="sr-only">Settings</SheetDescription>
         </SheetHeader>
         <div className="py-3.5">
           <SandboxSettings />
