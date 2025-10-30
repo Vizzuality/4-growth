35a8faf6337baea2465b9f0c3b946cbd363a6e26
atrincas <atrincas@gmail.com>
2025-10-30 13:53:50 +0100

    fe: update scenarios sheet for projections

--- client/src/containers/bottom-bar/projections/scenarios-sheet/index.tsx
+++ client/src/containers/bottom-bar/projections/scenarios-sheet/index.tsx
@@ -1,4 +1,5 @@
-import { FC } from "react";
+"use client";
+import { FC, useState } from "react";
 
 import ScenariosSelector from "@/containers/scenarios/selector";
 
@@ -6,22 +7,31 @@ import { Button } from "@/components/ui/button";
 import {
   Sheet,
   SheetContent,
+  SheetDescription,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
 
 const ScenariosSheet: FC = () => {
+  const [open, setOpen] = useState<boolean>(false);
+
   return (
-    <Sheet>
+    <Sheet open={open} onOpenChange={setOpen}>
       <SheetTrigger asChild>
         <Button className="w-full">Scenarios</Button>
       </SheetTrigger>
-      <SheetContent className="h-full w-screen bg-navy-900" side="bottom">
-        <SheetHeader className="mb-4">
-          <SheetTitle>Scenarios</SheetTitle>
+      <SheetContent
+        className="h-full max-h-[80%] w-screen rounded-t-2xl border-t-navy-900 bg-navy-900 px-0 pb-0"
+        side="bottom"
+      >
+        <SheetHeader className="mb-6">
+          <SheetTitle className="px-4 text-left text-base">
+            Scenarios
+          </SheetTitle>
+          <SheetDescription className="sr-only">Scenarios</SheetDescription>
         </SheetHeader>
-        <ScenariosSelector />
+        <ScenariosSelector onToggleScenario={() => setOpen(false)} />
       </SheetContent>
     </Sheet>
   );
