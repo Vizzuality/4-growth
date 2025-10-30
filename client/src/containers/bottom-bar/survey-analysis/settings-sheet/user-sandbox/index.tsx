0ff959f7f180caaeaa7c9146a15950c2feef2667
atrincas <atrincas@gmail.com>
2025-10-30 13:47:36 +0100

    fe: update settings sheet for survey analysis user sandbox page

--- client/src/containers/bottom-bar/survey-analysis/settings-sheet/user-sandbox/index.tsx
+++ client/src/containers/bottom-bar/survey-analysis/settings-sheet/user-sandbox/index.tsx
@@ -1,9 +1,13 @@
 "use client";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 
+import { WidgetVisualizationsType } from "@shared/dto/widgets/widget-visualizations.constants";
 import { useAtom, useAtomValue } from "jotai";
 
-import { normalizeWidgetData } from "@/lib/normalize-widget-data";
+import {
+  getResponseRate,
+  normalizeWidgetData,
+} from "@/lib/normalize-widget-data";
 import { client } from "@/lib/queryClient";
 import { queryKeys } from "@/lib/queryKeys";
 
@@ -12,13 +16,16 @@ import {
   sandboxFiltersAtom,
   sandboxVisualizationAtom,
   sandboxIndicatorAtom,
+  sandboxBreakdownAtom,
 } from "@/containers/sidebar/store";
 import VisualizationSelector from "@/containers/sidebar/visualization-selector";
 
 import { Button } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import {
   Sheet,
   SheetContent,
+  SheetDescription,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
@@ -27,48 +34,102 @@ import {
 const SettingsSheet: FC = () => {
   const filters = useAtomValue(sandboxFiltersAtom);
   const [indicator, setIndicator] = useAtom(sandboxIndicatorAtom);
+  const breakdown = useAtomValue(sandboxBreakdownAtom);
   const [visualization, setVisualization] = useAtom(sandboxVisualizationAtom);
-  const getWidgetQuery = client.widgets.getWidget.useQuery(
-    queryKeys.widgets.one(indicator || "", filters).queryKey,
+  const [open, setOpen] = useState(false);
+  const [newIndicator, setNewIndicator] = useState<string>("");
+  const [newVisualization, setNewVisualization] =
+    useState<WidgetVisualizationsType | null>(null);
+  const { data: widget } = client.widgets.getWidget.useQuery(
+    queryKeys.widgets.one(newIndicator, filters, breakdown || undefined)
+      .queryKey,
     {
-      params: { id: indicator as string },
+      params: { id: newIndicator },
       query: {
-        filters: filters,
+        filters,
+        breakdown: breakdown || undefined,
       },
     },
     {
-      enabled: !!indicator,
+      enabled: !!newIndicator,
+      // No retry to immediately show the final component or we have to add a spinner?
+      retry: 0,
       select: (res) => ({
         ...res.body.data,
         data: {
           raw: res.body.data.data,
           percentages: normalizeWidgetData(res.body.data.data),
         },
+        responseRate: getResponseRate(res.body.data.data),
       }),
     },
   );
+  const handleSubmitButtonClick = () => {
+    setIndicator(newIndicator);
+    setVisualization(newVisualization);
+    handleClose();
+  };
+  const handleClose = () => {
+    setOpen(false);
+    setNewIndicator("");
+    setNewVisualization(null);
+  };
+
+  useEffect(() => {
+    if (open) {
+      setNewIndicator(indicator || "");
+      setNewVisualization(visualization);
+    }
+  }, [open, indicator, visualization, setNewIndicator, setNewVisualization]);
+
+  useEffect(() => {
+    if (!widget) return;
+
+    if (
+      !newVisualization ||
+      !widget.visualisations.includes(newVisualization)
+    ) {
+      setNewVisualization(widget.defaultVisualization);
+    }
+  }, [widget, newVisualization, setNewVisualization]);
 
   return (
-    <Sheet>
+    <Sheet
+      open={open}
+      onOpenChange={(open) => {
+        if (open) {
+          setOpen(open);
+        } else {
+          handleClose();
+        }
+      }}
+    >
       <SheetTrigger asChild>
         <Button className="w-full">Settings</Button>
       </SheetTrigger>
-      <SheetContent className="h-full w-screen bg-navy-900" side="bottom">
-        <SheetHeader>
-          <SheetTitle>Settings</SheetTitle>
+      <SheetContent
+        className="flex h-full max-h-[80%] w-screen flex-col justify-between rounded-t-2xl border-t-navy-900 bg-navy-900 px-0 pb-0"
+        side="bottom"
+      >
+        <SheetHeader className="mb-6">
+          <SheetTitle className="px-4 text-left text-base">Settings</SheetTitle>
+          <SheetDescription className="sr-only">Settings</SheetDescription>
         </SheetHeader>
-        <div className="py-3.5">
+        <ScrollArea className="h-full">
           <VisualizationSelector
-            indicator={indicator}
-            visualization={visualization}
-            widget={getWidgetQuery.data}
-            onVisualizationSelected={setVisualization}
+            indicator={newIndicator}
+            visualization={newVisualization}
+            widget={widget}
+            onVisualizationSelected={setNewVisualization}
           />
           <IndicatorSelector
-            widget={getWidgetQuery.data}
-            onIndicatorSelected={setIndicator}
+            widget={widget}
+            onIndicatorSelected={setNewIndicator}
           />
-        </div>
+        </ScrollArea>
+        <Button className="w-full" onClick={handleSubmitButtonClick}>
+          Apply
+        </Button>
       </SheetContent>
     </Sheet>
   );
