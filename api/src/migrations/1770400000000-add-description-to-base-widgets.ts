a4530f5f6ce93e288e38134cb68324d6ade6e208
Manuel Escalante <maescalantehe@gmail.com>
2026-02-06 16:20:39 +0100

    feat: add description column to base_widgets table

---
+++ api/src/migrations/1770400000000-add-description-to-base-widgets.ts
@@ -0,0 +1,19 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AddDescriptionToBaseWidgets1770400000000
+  implements MigrationInterface
+{
+  name = 'AddDescriptionToBaseWidgets1770400000000';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "base_widgets" ADD "description" character varying`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "base_widgets" DROP COLUMN "description"`,
+    );
+  }
+}
