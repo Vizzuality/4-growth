f4c009804546fd3770a088fa61bf7ba8f760ed7e
atrincas <atrincas@gmail.com>
2026-02-23 17:41:19 +0100

    add category-selector to projections sandbox sidebar

--- client/src/containers/sidebar/projections-sidebar/sandbox-sidebar/index.tsx
+++ client/src/containers/sidebar/projections-sidebar/sandbox-sidebar/index.tsx
@@ -1,12 +1,14 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 
 import { ADD_FILTER_MODE } from "@/lib/constants";
 
+import useProjectionsCategoryFilter from "@/hooks/use-category-filter";
 import useFilters from "@/hooks/use-filters";
 
 import FilterSettings from "@/containers/sidebar/filter-settings";
 import ClearFiltersButton from "@/containers/sidebar/filter-settings/clear-filters-button";
 import { PROJECTIONS_DEFAULT_FILTERS } from "@/containers/sidebar/filter-settings/constants";
+import ProjectionsCategorySelector from "@/containers/sidebar/projections-sidebar/category-selector";
 import SandboxSettings from "@/containers/sidebar/projections-sidebar/sandbox-settings";
 
 import {
@@ -18,22 +20,51 @@ import {
 
 const SandboxSidebar: FC = () => {
   const { filters, addFilter, removeFilterValue } = useFilters();
+  const { selectedCategories, isCategorySelected } =
+    useProjectionsCategoryFilter();
+  const [openItems, setOpenItems] = useState<string[]>(
+    isCategorySelected ? ["settings", "filters"] : ["categories"],
+  );
+  const operationArea = selectedCategories[0];
+
   return (
     <Accordion
       key="sidebar-accordion-sandbox"
       type="multiple"
       className="w-full overflow-y-auto"
-      defaultValue={["settings"]}
+      value={openItems}
+      onValueChange={setOpenItems}
     >
+      <AccordionItem value="categories">
+        <AccordionTrigger>
+          <span className="mr-4 flex w-full items-center justify-between">
+            <span>Operation area</span>
+            {isCategorySelected && (
+              <span className="text-xs font-normal">{operationArea}</span>
+            )}
+          </span>
+        </AccordionTrigger>
+        <AccordionContent className="py-3.5">
+          <ProjectionsCategorySelector
+            onSelect={() =>
+              setOpenItems((prev) => prev.filter((v) => v !== "categories"))
+            }
+          />
+        </AccordionContent>
+      </AccordionItem>
       <AccordionItem value="settings">
-        <AccordionTrigger>Settings</AccordionTrigger>
+        <AccordionTrigger disabled={!isCategorySelected}>
+          Settings
+        </AccordionTrigger>
         <AccordionContent className="py-3.5">
           <SandboxSettings />
         </AccordionContent>
       </AccordionItem>
       <AccordionItem value="filters">
         <div className="relative">
-          <AccordionTrigger>Filters</AccordionTrigger>
+          <AccordionTrigger disabled={!isCategorySelected}>
+            Filters
+          </AccordionTrigger>
           <ClearFiltersButton />
         </div>
         <AccordionContent className="py-3.5">
