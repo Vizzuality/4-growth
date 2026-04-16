971a6276f5f1c792d06d89dfffcaa9f427b257b2
atrincas <atrincas@gmail.com>
2026-04-16 16:18:02 +0200

    feat: add scenarios multi-select filter to projections when visualization is table

--- client/src/containers/bottom-bar/projections/filter-settings/index.tsx
+++ client/src/containers/bottom-bar/projections/filter-settings/index.tsx
@@ -1,20 +1,34 @@
 "use client";
 import { FC, useMemo } from "react";
 
+import { PageFilter } from "@shared/dto/widgets/page-filter.entity";
+
 import { client } from "@/lib/queryClient";
 import { queryKeys } from "@/lib/queryKeys";
 import { normalizeProjectionsFilterValues } from "@/lib/utils";
 
+import useSettings from "@/hooks/use-settings";
+
 import FilterSettingsButton from "@/containers/bottom-bar/projections/filter-settings/button";
+import { SCENARIOS } from "@/containers/scenarios/constants";
 import { DEFAULT_FILTERS_LABEL_MAP } from "@/containers/sidebar/filter-settings/constants";
 
 import { filterProjectionsFilters } from "@/utils/filters";
+
+const SCENARIO_PAGE_FILTER = {
+  name: "scenario",
+  label: "Scenario",
+  values: SCENARIOS.map((s) => s.value),
+} as PageFilter;
 
 interface FilterSettingsProps {
   defaultFilters: string[];
 }
 
 const FilterSettings: FC<FilterSettingsProps> = ({ defaultFilters }) => {
+  const { settings } = useSettings();
+  const isTableVisualization = settings !== null && "table" in settings;
+
   const projectionsFiltersQuery =
     client.projections.getProjectionsFilters.useQuery(
       queryKeys.projections.filters.queryKey,
@@ -35,13 +49,21 @@ const FilterSettings: FC<FilterSettingsProps> = ({ defaultFilters }) => {
   );
 
   const effectiveAllFilters = useMemo(() => {
+    let filters;
     if (customFilters.length <= 1) {
-      return allFilters;
+      filters = allFilters;
+    } else {
+      filters = allFilters.filter((pageFilter) =>
+        defaultFilters.includes(pageFilter.name),
+      );
     }
-    return allFilters.filter((pageFilter) =>
-      defaultFilters.includes(pageFilter.name),
-    );
-  }, [allFilters, defaultFilters, customFilters]);
+
+    if (isTableVisualization) {
+      return [...filters, SCENARIO_PAGE_FILTER];
+    }
+
+    return filters;
+  }, [allFilters, defaultFilters, customFilters, isTableVisualization]);
 
   return (
     <div className="">
