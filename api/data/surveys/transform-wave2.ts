5d6cd98badee508b15eeb0f067a165b2396614e4
Mauricio Fragachan <mauricio@acidtango.com>
2026-08-11 09:37:33 -0400

    fix(wave2/transform): use Level3 as canonical question text for Wave 2 hierarchy

--- api/data/surveys/transform-wave2.ts
+++ api/data/surveys/transform-wave2.ts
@@ -1,7 +1,7 @@
 import { transform } from './transform';
 
 export const transformWave2 = () =>
-  transform('data/surveys/wave2', `${__dirname}/surveys-wave2.json`);
+  transform('data/surveys/wave2', `${__dirname}/surveys-wave2.json`, 'level3');
 
 if (require.main === module) {
   void transformWave2();
